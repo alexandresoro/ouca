69c964048c37d0216133848427d6f6a0722ef0a6
Camille Carrier <camille@OrdinateCamille.lan>
2019-03-22 11:33:27 +0100

    Create constants file

---
+++ src/utils/constants.ts
@@ -0,0 +1,26 @@
+export const NB_DONNEES: string = "nbDonnees";
+export const NB_COMMUNES: string = "nbCommunes";
+export const NB_LIEUXDITS: string = "nbLieuxdits";
+export const NB_ESPECES: string = "nbEspeces";
+
+export const TABLE_OBSERVATEUR: string = "observateur";
+export const TABLE_DEPARTEMENT: string = "departement";
+export const TABLE_COMMUNE: string = "commune";
+export const TABLE_LIEUDIT: string = "lieudit";
+export const TABLE_METEO: string = "meteo";
+export const TABLE_CLASSE: string = "classe";
+export const TABLE_ESPECE: string = "espece";
+export const TABLE_AGE: string = "age";
+export const TABLE_SEXE: string = "sexe";
+export const TABLE_ESTIMATION_NOMBRE: string = "estimation_nombre";
+export const TABLE_ESTIMATION_DISTANCE: string = "estimation_distance";
+export const TABLE_COMPORTEMENT: string = "comportement";
+export const TABLE_MILIEU: string = "milieu";
+
+export const COLUMN_CODE: string = "code";
+export const COLUMN_ESPECE_ID: string = "espece_id";
+export const COLUMN_LIBELLE: string = "libelle";
+export const COLUMN_NOM: string = "nom";
+
+export const ORDER_ASC: string = "ASC";
+export const ORDER_DESC: string = "DESC";
