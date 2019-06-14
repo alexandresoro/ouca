90e9996201942640a59c64254e6da4f48dfbecbf
Alexandre Soro <code@soro.email>
2019-06-14 20:26:59 +0200

    Add model for import response

---
+++ import-response.object.ts
@@ -0,0 +1,11 @@
+export interface ImportResponse {
+  isSuccessful: boolean;
+
+  reasonForFailure?: string;
+
+  numberOfLinesExtracted?: number;
+
+  numberOfLinesFailedToImport?: number;
+
+  errorFileName?: string;
+}
