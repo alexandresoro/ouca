b0caba7b00bf5f4605cbf86552b4b44f29bbb09f
Camille Carrier <camille.carrier@outlook.com>
2019-06-14 12:44:37 +0200

    create entry point for import

--- src/requests/import.ts
+++ src/requests/import.ts
@@ -16,3 +16,101 @@ export const importFile = async (
   });
   console.log("toto");
 };
+
+export const importObservateurs = async (
+  isMockDatabaseMode: boolean,
+  httpParameters: HttpParameters
+): Promise<any> => {
+  // TODO
+};
+
+export const importDepartements = async (
+  isMockDatabaseMode: boolean,
+  httpParameters: HttpParameters
+): Promise<any> => {
+  // TODO
+};
+
+export const importCommunes = async (
+  isMockDatabaseMode: boolean,
+  httpParameters: HttpParameters
+): Promise<any> => {
+  // TODO
+};
+
+export const importLieuxdits = async (
+  isMockDatabaseMode: boolean,
+  httpParameters: HttpParameters
+): Promise<any> => {
+  // TODO
+};
+
+export const importMeteos = async (
+  isMockDatabaseMode: boolean,
+  httpParameters: HttpParameters
+): Promise<any> => {
+  // TODO
+};
+
+export const importClasses = async (
+  isMockDatabaseMode: boolean,
+  httpParameters: HttpParameters
+): Promise<any> => {
+  // TODO
+};
+
+export const importEspeces = async (
+  isMockDatabaseMode: boolean,
+  httpParameters: HttpParameters
+): Promise<any> => {
+  // TODO
+};
+
+export const importAges = async (
+  isMockDatabaseMode: boolean,
+  httpParameters: HttpParameters
+): Promise<any> => {
+  // TODO
+};
+
+export const importSexes = async (
+  isMockDatabaseMode: boolean,
+  httpParameters: HttpParameters
+): Promise<any> => {
+  // TODO
+};
+
+export const importEstimationsNombre = async (
+  isMockDatabaseMode: boolean,
+  httpParameters: HttpParameters
+): Promise<any> => {
+  // TODO
+};
+
+export const importEstimationsDistance = async (
+  isMockDatabaseMode: boolean,
+  httpParameters: HttpParameters
+): Promise<any> => {
+  // TODO
+};
+
+export const importComportements = async (
+  isMockDatabaseMode: boolean,
+  httpParameters: HttpParameters
+): Promise<any> => {
+  // TODO
+};
+
+export const importMilieux = async (
+  isMockDatabaseMode: boolean,
+  httpParameters: HttpParameters
+): Promise<any> => {
+  // TODO
+};
+
+export const importDonnees = async (
+  isMockDatabaseMode: boolean,
+  httpParameters: HttpParameters
+): Promise<any> => {
+  // TODO
+};
