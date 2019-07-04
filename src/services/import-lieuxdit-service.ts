804e00b16954e149e2f94ff8354bc98963dc8d9d
Camille CARRIER <camille.carrier@amadeus.com>
2019-07-04 13:09:29 +0200

    Continue import lieudit service

--- src/services/import-lieuxdit-service.ts
+++ src/services/import-lieuxdit-service.ts
@@ -1,7 +1,10 @@
-import { Observateur } from "../basenaturaliste-model/observateur.object";
 import { ImportService } from "./import-service";
+import { SqlConnection } from "../sql/sql-connection";
+import { getEntiteAvecLibelleByLibelleQuery } from "../sql/sql-queries-utils";
+import { Lieudit } from "../basenaturaliste-model/lieudit.object";
 
-export class ImportLieuxditService {
+export class ImportLieuxditService extends ImportService  {
+  
   private DEPARTEMENT_INDEX: number = 0;
   private CODE_COMMUNE_INDEX: number = 1;
   private NOM_COMMUNE_INDEX: number = 2;
@@ -10,36 +13,171 @@ export class ImportLieuxditService {
   private LONGITUDE_INDEX: number = 5;
   private LATITUDE_INDEX: number = 6;
 
-  protected saveObject(objectTab: string[]): void {
-    // TODO
-  }
-
   protected getNumberOfColumns = () => {
     return 7;
   }
 
-  protected getObject = (objectTab: string[]): Observateur => {
+  protected getEntity = (entityTab: string[]): Lieudit => {
+    const communeId = 0; // TODO
+    
     return {
       id: null,
-      libelle: objectTab[this.DEPARTEMENT_INDEX]
+      communeId,
+      nom: entityTab[this.NOM_INDEX].trim(),
+      altitude: +entityTab[this.ALTITUDE_INDEX].trim(),
+      longitude: +entityTab[this.LONGITUDE_INDEX].trim(),
+      latitude: +entityTab[this.LATITUDE_INDEX].trim()
     };
   }
 
-  protected isObjectValid = (objectTab: string[]): boolean => {
-    return this.isLibelleValid(objectTab[this.DEPARTEMENT_INDEX]);
-  }
-
-  private isLibelleValid = (libelle: string): boolean => {
-    if (!libelle) {
-      // this.message = "Le libellé ne peut pas être vide";
+  protected isEntityValid = async (entityTab: string[]): Promise<boolean> => {
+    if ( !this.isDepartementValid(entityTab[this.DEPARTEMENT_INDEX]) 
+        || !this.isCodeCommuneValid(entityTab[this.CODE_COMMUNE_INDEX]) 
+        || !this.isNomCommuneValid(entityTab[this.NOM_COMMUNE_INDEX]) 
+        || !this.isNomLieuditValid(entityTab[this.NOM_INDEX])
+        || !this.isAltitudeValid(entityTab[this.ALTITUDE_INDEX]) 
+        || !this.isLongitudeValid(entityTab[this.LONGITUDE_INDEX])
+        || !this.isLatitudeValid(entityTab[this.LATITUDE_INDEX])) {
       return false;
     }
 
-    if (libelle.length > 100) {
-      // this.message = "La longueur maximale du libellé est 100 caractères";
+    // TO DO check that the departement exists
+
+    // TO DO check that the commune exists
+
+    // Check that the lieu-dit does not exist yet
+    return !(await this.isExistingEntity(entityTab));
+  }
+
+  protected isExistingEntity = async (
+    entityTab: string[]
+  ): Promise<boolean> => {
+    const results = await SqlConnection.query(
+      getEntiteAvecLibelleByLibelleQuery(
+        "observateur",
+        entityTab[this.NOM_INDEX]
+      )
+    );
+
+    if (results && results[0] && results[0].id) {
+      // The entity already exists
+      this.message = "Il existe déjà un observateur avec ce libellé.";
+      return true;
+    }
+
+    return false;
+  }
+
+  private isDepartementValid = (departement: string): boolean => {
+    departement = departement.trim();
+
+    if (!departement) {
+      this.message = "Le département du lieu-dit ne peut pas être vide";
+      return false;
+    }
+    return true;
+  }
+
+  private isCodeCommuneValid = (code: string): boolean => {
+    code = code.trim();
+
+    if (!code) {
+      this.message = "Le code de la commune du lieu-dit ne peut pas être vide";
+      return false;
+    }
+    
+    if(!Number.isInteger(Number(code))) {
+      this.message = "Le code de la commune du lieu-dit doit être un entier";
       return false;
     }
 
     return true;
   }
+
+  private isNomCommuneValid = (nom: string): boolean => {
+    nom = nom.trim();
+
+    if (!nom) {
+      this.message = "Le nom de la commune du lieu-dit ne peut pas être vide";
+      return false;
+    }
+
+    return true;
+  }
+
+  private isNomLieuditValid = (nom: string): boolean => {
+    nom = nom.trim();
+
+    if (!nom) {
+      this.message = "Le nom du lieu-dit ne peut pas être vide";
+      return false;
+    }
+
+    if (nom.length > 150) {
+      this.message = "La longueur maximale du nom du lieu-dit est de 150 caractères";
+      return false;
+    }
+
+    return true;
+  }
+
+  private isAltitudeValid(altitudeStr: string) {
+    if (!altitudeStr) {
+      this.message = "L'altitude du lieu-dit ne peut pas être vide";
+      return false;
+    }
+    
+    const altitude: number = Number(altitudeStr);
+
+    if(!Number.isInteger(altitude)) {
+      this.message = "L'altitude du lieu-dit doit être un entier";
+      return false;
+    }
+
+    if(altitude < 0 || altitude > 99999) {
+      this.message = "L'altitude du lieu-dit doit être un entier compris entre 0 et 99999";
+      return false;
+    }
+
+    return true;
+  }
+
+  private isLongitudeValid(longitudeStr: string) {
+    if (!longitudeStr) {
+      this.message = "La longitude du lieu-dit ne peut pas être vide";
+      return false;
+    }
+    
+    const longitude: number = Number(longitudeStr);
+
+    if(!Number.isInteger(longitude)) {
+      this.message = "La longitude du lieu-dit doit être un entier";
+      return false;
+    }
+
+    if(longitude < 0 || longitude > 99999999) {
+      this.message = "La longitude du lieu-dit doit être un entier compris entre 0 et 99999999";
+      return false;
+    }
+  }
+
+  private isLatitudeValid(latitudeStr: string) {
+    if (!latitudeStr) {
+      this.message = "La latitude du lieu-dit ne peut pas être vide";
+      return false;
+    }
+    
+    const latitude: number = Number(latitudeStr);
+
+    if(!Number.isInteger(latitude)) {
+      this.message = "La latitude du lieu-dit doit être un entier";
+      return false;
+    }
+
+    if(latitude < 0 || latitude > 99999999) {
+      this.message = "La latitude du lieu-dit doit être un entier compris entre 0 et 99999999";
+      return false;
+    }
+  }
+  
 }
