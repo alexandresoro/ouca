4d065aba2a35cb4382bd4bd2f8567f47f1332e9f
Camille Carrier <camille.carrier@outlook.com>
2020-04-17 15:25:03 +0200

    Remove useless method getOriginCoordinates

--- src/coordinates-system/coordinates-helper.ts
+++ src/coordinates-system/coordinates-helper.ts
@@ -3,16 +3,9 @@ import { EntityWithCoordinates } from "../entity-with-coordinates.model";
 import { CoordinatesSystemType } from "./coordinates-system.object";
 import { transformCoordinates } from "./coordinates-transformer";
 
-export const getOriginCoordinates = (
-  object: EntityWithCoordinates
-): Coordinates => {
-  return object?.coordinates ? object.coordinates : null;
-};
-
 export const getCoordinates = (
   object: EntityWithCoordinates,
   coordinatesSystem: CoordinatesSystemType
 ): Coordinates => {
-  const originCoordinates: Coordinates = getOriginCoordinates(object);
-  return transformCoordinates(originCoordinates, coordinatesSystem);
+  return transformCoordinates(object.coordinates, coordinatesSystem);
 };
