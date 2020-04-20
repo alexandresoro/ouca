acebe6913f0c179720684f5155dffdff18c2eff3
Camille Carrier <camille.carrier@outlook.com>
2020-04-20 19:08:46 +0200

    Fix get all estimation nombre query

--- src/sql/sql-queries-estimation-nombre.ts
+++ src/sql/sql-queries-estimation-nombre.ts
@@ -1,6 +1,10 @@
 import { EstimationNombreDb } from "../objects/db/estimation-nombre-db.object";
 import { NumberOfObjectsById } from "../objects/number-of-objects-by-id.object";
-import { COLUMN_LIBELLE, ORDER_ASC, TABLE_AGE } from "../utils/constants";
+import {
+  COLUMN_LIBELLE,
+  ORDER_ASC,
+  TABLE_ESTIMATION_NOMBRE
+} from "../utils/constants";
 import { queryToFindNumberOfDonneesByDonneeEntityId } from "./sql-queries-donnee";
 import { queryToFindAllEntities } from "./sql-queries-utils";
 
@@ -8,7 +12,7 @@ export const queryToFindAllEstimationsNombre = async (): Promise<
   EstimationNombreDb[]
 > => {
   return queryToFindAllEntities<EstimationNombreDb>(
-    TABLE_AGE,
+    TABLE_ESTIMATION_NOMBRE,
     COLUMN_LIBELLE,
     ORDER_ASC
   );
