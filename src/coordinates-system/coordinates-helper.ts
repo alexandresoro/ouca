f11e676c06e3aff01a0b1849955118d612db396b
Camille Carrier <camille.carrier@outlook.com>
2020-04-25 15:58:14 +0200

    Move coordinates customized logic into common

--- src/coordinates-system/coordinates-helper.ts
+++ src/coordinates-system/coordinates-helper.ts
@@ -1,5 +1,6 @@
 import { Coordinates } from "../coordinates.object";
 import { EntityWithCoordinates } from "../entity-with-coordinates.model";
+import { Lieudit } from "../lieudit.model";
 import { CoordinatesSystemType } from "./coordinates-system.object";
 import { transformCoordinates } from "./coordinates-transformer";
 
@@ -8,4 +9,26 @@ export const getCoordinates = (
   coordinatesSystem: CoordinatesSystemType
 ): Coordinates => {
   return transformCoordinates(object.coordinates, coordinatesSystem);
+};
+
+export const areCoordinatesCustomized = (
+  lieudit: Lieudit,
+  altitude: number,
+  longitude: number,
+  latitude: number,
+  system: CoordinatesSystemType
+): boolean => {
+  if (lieudit?.id) {
+    const lieuditCoordinates: Coordinates = getCoordinates(lieudit, system);
+
+    if (
+      lieudit.altitude !== altitude ||
+      lieuditCoordinates.longitude !== longitude ||
+      lieuditCoordinates.latitude !== latitude
+    ) {
+      return true;
+    }
+  }
+
+  return false;
 };
