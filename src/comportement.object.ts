e1b64200185710d428ee2a59275804cefd1d527f
Camille Carrier <camille.carrier@outlook.com>
2020-04-29 15:22:35 +0200

    Set isNicheur as a mandatory attribute of comportements

--- src/comportement.object.ts
+++ src/comportement.object.ts
@@ -1,5 +1,5 @@
 import { EntiteAvecLibelleEtCode } from "./entite-avec-libelle-et-code.object";
 
 export interface Comportement extends EntiteAvecLibelleEtCode {
-  isNicheur?: boolean;
+  isNicheur: boolean;
 }
