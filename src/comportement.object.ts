ee673364f789d5689bc7c53107e9dd46ff222a69
Camille Carrier <camille.carrier@outlook.com>
2020-04-29 14:12:04 +0200

    Add is nicheur in comportement

--- src/comportement.object.ts
+++ src/comportement.object.ts
@@ -1,3 +1,5 @@
 import { EntiteAvecLibelleEtCode } from "./entite-avec-libelle-et-code.object";
 
-export interface Comportement extends EntiteAvecLibelleEtCode {}
+export interface Comportement extends EntiteAvecLibelleEtCode {
+  isNicheur?: boolean;
+}
