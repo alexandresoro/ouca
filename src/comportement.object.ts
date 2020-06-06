792dca168f57ac53a9cfddac4a4338350740b71c
Camille Carrier <camille.carrier@outlook.com>
2020-06-06 10:14:14 +0200

    Remove isNicheur from comportement

--- src/comportement.object.ts
+++ src/comportement.object.ts
@@ -1,8 +1,6 @@
 import { EntiteAvecLibelleEtCode } from "./entite-avec-libelle-et-code.object";
-import { Nicheur, NicheurCode } from "./nicheur.model";
+import { NicheurCode } from "./nicheur.model";
 
 export interface Comportement extends EntiteAvecLibelleEtCode {
-  isNicheur?: boolean;
-
-  nicheur: Nicheur | NicheurCode;
+  nicheur: NicheurCode;
 }
