74a3dffd3d394a5958940c8bf37fa2767315bb3f
Camille Carrier <camille.carrier@outlook.com>
2020-06-06 09:40:54 +0200

    Create nicheur model

--- src/comportement.object.ts
+++ src/comportement.object.ts
@@ -1,5 +1,8 @@
 import { EntiteAvecLibelleEtCode } from "./entite-avec-libelle-et-code.object";
+import { Nicheur, NicheurCode } from "./nicheur.model";
 
 export interface Comportement extends EntiteAvecLibelleEtCode {
-  isNicheur: boolean;
+  isNicheur?: boolean;
+
+  nicheur: Nicheur | NicheurCode;
 }
