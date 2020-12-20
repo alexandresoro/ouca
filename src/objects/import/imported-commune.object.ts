031baae105954bfffb6a076ebfeb72b81b7694a4
Camille Carrier <camille.carrier@outlook.com>
2020-12-20 21:28:31 +0100

    Remove papaparse types

---
+++ src/objects/import/imported-commune.object.ts
@@ -0,0 +1,88 @@
+import { Commune } from "@ou-ca/ouca-model/commune.model";
+
+const DEPARTEMENT_INDEX = 0;
+const CODE_INDEX = 1;
+const NOM_INDEX = 2;
+
+const CODE_MIN_VALUE = 0;
+const CODE_MAX_VALUE = 65535;
+const NOM_MAX_LENGTH = 100;
+
+export class ImportedCommune {
+
+  departement: string;
+  code: string;
+  nom: string;
+
+  constructor(communeTab: string[]) {
+    this.departement = communeTab[DEPARTEMENT_INDEX].trim();
+    this.code = communeTab[CODE_INDEX].trim();
+    this.nom = communeTab[NOM_INDEX].trim();
+  }
+
+  buildCommune = (
+    departementId: number
+  ): Commune => {
+    return {
+      id: null,
+      departementId,
+      code: +this.code,
+      nom: this.nom
+    };
+  };
+
+  checkValidity = (): string => {
+    const departementError = this.checkDepartementValidity();
+    if (departementError) {
+      return departementError;
+    }
+
+    const codeCommuneError = this.checkCodeCommuneValidity();
+    if (codeCommuneError) {
+      return codeCommuneError;
+    }
+
+    const nomCommuneError = this.checkNomCommuneValidity();
+    if (nomCommuneError) {
+      return nomCommuneError;
+    }
+  }
+
+  private checkDepartementValidity = (): string => {
+    return this.departement ? null : "Le département de la commune ne peut pas être vide";
+  };
+
+  private checkCodeCommuneValidity = (): string => {
+    if (!this.code) {
+      return "Le code de la commune ne peut pas être vide";
+    }
+
+    const codeCommune = Number(this.code);
+
+    if (!Number.isInteger(codeCommune)) {
+      return "Le code de la commune doit être un entier";
+    }
+
+    if (
+      codeCommune < CODE_MIN_VALUE ||
+      codeCommune > CODE_MAX_VALUE
+    ) {
+      return `Le code de la commune doit être un entier compris entre ${CODE_MIN_VALUE} et ${CODE_MAX_VALUE}`;
+    }
+
+    return null;
+  };
+
+  private checkNomCommuneValidity = (): string => {
+    if (!this.nom) {
+      return "Le nom de la commune ne peut pas être vide";
+    }
+
+    if (this.nom.length > NOM_MAX_LENGTH) {
+      return `La longueur maximale du nom de la commune est de ${NOM_MAX_LENGTH} caractères`;
+    }
+
+    return null;
+  };
+
+}
---
+++ src/objects/import/imported-departement.object.ts
@@ -0,0 +1,40 @@
+import { Departement } from "@ou-ca/ouca-model/departement.object";
+
+const CODE_INDEX = 0;
+const CODE_MAX_LENGTH = 100;
+
+export class ImportedDepartement {
+
+  code: string;
+
+  constructor(departementTab: string[]) {
+    this.code = departementTab[CODE_INDEX].trim();
+  }
+
+  buildDepartement = (): Departement => {
+    return {
+      id: null,
+      code: this.code
+    };
+  };
+
+  checkValidity = (): string => {
+    const codeError = this.checkCodeValidity();
+    if (codeError) {
+      return codeError;
+    }
+    return null;
+  }
+
+  private checkCodeValidity = (): string => {
+    if (!this.code) {
+      return "Le département ne peut pas être vide";
+    }
+
+    if (this.code.length > CODE_MAX_LENGTH) {
+      return `La longueur maximale du département est de ${CODE_MAX_LENGTH} caractères`;
+    }
+
+    return null;
+  };
+}
