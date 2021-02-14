7559c46ad29a3b7c04985c5da4816fab3a534c9e
Alexandre Soro <code@soro.email>
2021-02-14 21:19:27 +0100

    Add import message type to websocket types

--- src/websocket/websocket-message-type.model.ts
+++ src/websocket/websocket-message-type.model.ts
@@ -1,9 +1,11 @@
 export const UPDATE = "update";
 
+export const IMPORT = "import"; // Channel that is used to handle messages related to the import functionality
+
 export const OTHER = "other";
 
 export const TEXT = "text";
 
-const WEBSOCKET_MESSAGE_TYPE = [UPDATE, OTHER, TEXT] as const;
+const WEBSOCKET_MESSAGE_TYPE = [UPDATE, IMPORT, OTHER, TEXT] as const;
 
 export type WebSocketMessageType = typeof WEBSOCKET_MESSAGE_TYPE[number];
