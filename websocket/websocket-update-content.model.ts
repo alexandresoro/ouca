7c0eb16b2dfc0a2ccb0935da591a47c969a02cb6
Alexandre Soro <code@soro.email>
2021-09-25 03:04:34 +0200

    Update model

--- websocket/websocket-update-content.model.ts
+++ websocket/websocket-update-content.model.ts
@@ -6,13 +6,10 @@ import { Departement } from "../types/departement.object";
 import { Espece } from "../types/espece.model";
 import { EstimationDistance } from "../types/estimation-distance.object";
 import { EstimationNombre } from "../types/estimation-nombre.object";
-import { Lieudit } from "../types/lieudit.model";
 import { Milieu } from "../types/milieu.object";
 import { Sexe } from "../types/sexe.object";
 
 export interface WebsocketUpdateContent {
-  lieuxdits?: Lieudit[];
-
   communes?: Commune[];
 
   departements?: Departement[];
