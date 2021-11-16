de7ad1b68341216562b5c8b899e6d9167219362c
Alexandre Soro <code@soro.email>
2021-11-16 23:45:13 +0100

    Add helper to compute proper nicheur status

---
+++ helpers/nicheur-helper.ts
@@ -0,0 +1,25 @@
+import { NicheurCode, NICHEUR_VALUES } from "../types/nicheur.model";
+
+export const getNicheurStatusToDisplay = (comportements: { nicheur: NicheurCode | null }[], noNicheurFoundText: string): string => {
+
+  // Compute nicheur status for the Donnée (i.e. highest nicheur status of the comportements)
+  // First we keep only the comportements having a nicheur status
+  const nicheurStatuses: NicheurCode[] = comportements?.filter(
+    (comportement) => {
+      return !!comportement.nicheur;
+    }
+  ).map(
+    (comportement) => {
+      return comportement.nicheur;
+    }
+  ) ?? [];
+
+  // Then we keep the highest nicheur status
+  const nicheurStatusCode = nicheurStatuses?.length && nicheurStatuses.reduce(
+    (nicheurStatusOne, nicheurStatusTwo) => {
+      return NICHEUR_VALUES[nicheurStatusOne].weight >= NICHEUR_VALUES[nicheurStatusTwo].weight ? nicheurStatusOne : nicheurStatusTwo
+    }
+  );
+
+  return nicheurStatusCode ? NICHEUR_VALUES[nicheurStatusCode].name : noNicheurFoundText;
+}
