7441580a2f0f73c0bcb054849a66cd2575178acd
Alexandre Soro <code@soro.email>
2021-12-02 23:22:40 +0100

    Cleanup WS heartbeat message

--- websocket/websocket-message-type.model.ts
+++ websocket/websocket-message-type.model.ts
@@ -1,7 +1,3 @@
 export const IMPORT = "import"; // Channel that is used to handle messages related to the import functionality
 
-export const HEARTBEAT = "heartbeat"; // Channel used for ping/pong messages
-
-const WEBSOCKET_MESSAGE_TYPE = [IMPORT, HEARTBEAT] as const;
-
-export type WebSocketMessageType = typeof WEBSOCKET_MESSAGE_TYPE[number];
+export type WebSocketMessageType = typeof IMPORT;
