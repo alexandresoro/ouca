f565f623273b831ab34d789f6d3757e62fb6ed41
Alexandre Soro <code@soro.email>
2021-12-11 20:31:43 +0100

    Add linting rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App(): ReactElement {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,5 +22,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
