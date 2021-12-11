e4b341347fd8c54d021898d6da8898bf69990ab5
Alexandre Soro <code@soro.email>
2021-12-11 21:11:21 +0100

    Add first empty route

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React, { ReactElement } from 'react';
-import './App.css';
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
 
 export default function App(): ReactElement {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/login" element={<LoginPage />}></Route>
+    </Routes>
   );
 }
---
+++ src/components/LoginPage.tsx
@@ -0,0 +1,8 @@
+import { ReactElement } from "react";
+
+export default function LoginPage(): ReactElement {
+  return (
+    <>
+    </>
+  )
+}
