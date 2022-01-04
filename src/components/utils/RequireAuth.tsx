07fbc95cc420840fe330f6636b2856f0d8b56f1b
Alexandre Soro <code@soro.email>
2022-01-04 23:40:36 +0100

    Try to fix case where user is logged but refreshes

--- src/components/utils/RequireAuth.tsx
+++ src/components/utils/RequireAuth.tsx
@@ -1,16 +1,66 @@
-import { ReactElement, useContext } from "react";
-import { Navigate, useLocation } from "react-router-dom";
+import { gql, useMutation } from "@apollo/client";
+import { CircularProgress } from "@mui/material";
+import { ReactElement, useContext, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { UserContext } from "../../contexts/UserContext";
+import { UserInfo } from "../../model/graphql";
+import CenteredFlexBox from "./CenteredFlexBox";
+
+type RefreshTokenResult = {
+  userRefresh: UserInfo | null;
+};
+
+const REFRESH_TOKEN_MUTATION = gql`
+  mutation RefreskToken {
+    userRefresh {
+      id
+      username
+      firstName
+      lastName
+      role
+    }
+  }
+`;
 
 export default function RequireAuth(props: { children: ReactElement }) {
   const { children } = props;
 
-  const { userInfo } = useContext(UserContext);
+  const { userInfo, setUserInfo } = useContext(UserContext);
+
+  const navigate = useNavigate();
   const location = useLocation();
 
-  if (!userInfo) {
-    return <Navigate to="/login" state={{ from: location }} replace />;
+  const [refreshToken] = useMutation<RefreshTokenResult>(REFRESH_TOKEN_MUTATION);
+
+  useEffect(() => {
+    // If the user context is not defined, try to retrieve a valid token
+    if (!userInfo) {
+      refreshToken()
+        .then(({ data, errors }) => {
+          if (data?.userRefresh && !errors) {
+            setUserInfo(data.userRefresh);
+          } else {
+            navigate("/login", { replace: true, state: { from: location } });
+          }
+        })
+        .catch(() => {
+          navigate("/login", { replace: true, state: { from: location } });
+        });
+    }
+  }, [location, userInfo, setUserInfo, refreshToken, navigate]);
+
+  if (userInfo) {
+    // If the user is properly logged in, simply return the content
+    return children;
   }
 
-  return children;
+  return (
+    <CenteredFlexBox
+      sx={{
+        height: "100vh"
+      }}
+    >
+      <CircularProgress />
+    </CenteredFlexBox>
+  );
 }
