eec1faf418406066b85012b178c81a82a872aa32
Alexandre Soro <code@asoro.dev>
2022-02-21 21:20:19 +0100

    Add prisma mock for tests

---
+++ src/sql/prisma-mock.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+import { DeepMockProxy, mockDeep, mockReset } from "jest-mock-extended";
+import prisma from "./prisma";
+
+jest.mock("./prisma", () => ({
+  __esModule: true,
+  default: mockDeep<PrismaClient>()
+}));
+
+beforeEach(() => {
+  mockReset(prismaMock);
+});
+
+export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>;
