f625c07bd2e7a92dc602dcd911af5b9ebd07a532
Alexandre Soro <code@asoro.dev>
2022-03-12 17:47:17 +0100

    Add tests for sql query utils

--- src/services/entities/entities-utils.spec.ts
+++ src/services/entities/entities-utils.spec.ts
@@ -1,7 +1,7 @@
-import { DatabaseRole } from "@prisma/client";
+import { DatabaseRole, Prisma } from "@prisma/client";
 import { mock } from "jest-mock-extended";
 import { LoggedUser } from "../../types/LoggedUser";
-import { isEntityReadOnly } from "./entities-utils";
+import { getSqlPagination, getSqlSorting, isEntityReadOnly } from "./entities-utils";
 
 test("should return correct readonly status for non logged user", () => {
   const entity = mock<{ ownerId: string }>();
@@ -80,4 +80,76 @@ test("should return correct readonly status for admin and no entity owner", () =
   };
 
   expect(isEntityReadOnly(entity, user)).toBe(false);
+});
+
+test("should return correct SQL pagination with null input", () => {
+  expect(getSqlPagination(null)).toBe(Prisma.empty);
+});
+
+test("should return correct SQL pagination with undefined input", () => {
+  expect(getSqlPagination(undefined)).toBe(Prisma.empty);
+});
+
+test("should return correct SQL pagination with empty object input", () => {
+  expect(getSqlPagination({})).toBe(Prisma.empty);
+});
+
+test("should return correct SQL pagination with missing page size input", () => {
+  expect(
+    getSqlPagination({
+      pageNumber: 1
+    })
+  ).toBe(Prisma.empty);
+});
+
+test("should return correct SQL pagination with missing page number input", () => {
+  expect(
+    getSqlPagination({
+      pageSize: 20
+    })
+  ).toBe(Prisma.empty);
+});
+
+test("should return correct SQL pagination with valid input", () => {
+  expect(
+    getSqlPagination({
+      pageNumber: 3,
+      pageSize: 20
+    }).text
+  ).toEqual("LIMIT $1 OFFSET $2");
+  expect(
+    getSqlPagination({
+      pageNumber: 3,
+      pageSize: 20
+    }).values
+  ).toEqual([20, 60]);
+});
+
+test("should return correct SQL sorting with empty object input", () => {
+  expect(getSqlSorting({})).toBe(Prisma.empty);
+});
+
+test("should return correct SQL sorting with missing order in input", () => {
+  expect(
+    getSqlSorting({
+      sortOrder: "asc"
+    })
+  ).toBe(Prisma.empty);
+});
+
+test("should return correct SQL sorting with missing sort in input", () => {
+  expect(
+    getSqlSorting({
+      orderBy: "toto"
+    }).text
+  ).toEqual("ORDER BY toto asc");
+});
+
+test("should return correct SQL sorting with all fields in input", () => {
+  expect(
+    getSqlSorting({
+      orderBy: "toto",
+      sortOrder: "desc"
+    }).text
+  ).toEqual("ORDER BY toto desc");
 });
