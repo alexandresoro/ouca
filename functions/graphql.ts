8e344a5e28b45f7b98d4610490d9f7b5a428fbe5
Alexandre Soro <code@asoro.dev>
2022-04-11 16:46:02 +0200

    Test to check 308 behavior on GET

--- functions/graphql.ts
+++ functions/graphql.ts
@@ -1,3 +1,7 @@
 import { onRequestCommon } from "./_common";
 
 export const onRequestPost = onRequestCommon;
+
+export const onRequestGet: PagesFunction<{ API_URLS?: KVNamespace }> = async ({ request, env, next }) => {
+  return Response.redirect(`https://www.google.com`, 308);
+};
