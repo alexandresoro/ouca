0658076da8f903162824a069404780df70a876df
Alexandre Soro <code@asoro.dev>
2022-04-11 16:53:54 +0200

    Revert "Test to check 308 behavior on GET"
    This reverts commit 8e344a5e28b45f7b98d4610490d9f7b5a428fbe5.

--- functions/graphql.ts
+++ functions/graphql.ts
@@ -1,7 +1,3 @@
 import { onRequestCommon } from "./_common";
 
 export const onRequestPost = onRequestCommon;
-
-export const onRequestGet: PagesFunction<{ API_URLS?: KVNamespace }> = async ({ request, env, next }) => {
-  return Response.redirect(`https://www.google.com`, 308);
-};
