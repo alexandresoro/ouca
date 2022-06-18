94b7a98a75b8d249dbde284fe6745e46f3318a56
Alexandre Soro <code@asoro.dev>
2022-06-18 23:41:08 +0200

    Another test

--- functions/test.ts
+++ functions/test.ts
@@ -1,5 +1,14 @@
 export const onRequestGet: PagesFunction = async ({ request }) => {
-  const headers = request.headers;
+  const headersIt = request.headers.entries();
+
+  let headers = {};
+
+  let result = headersIt.next();
+  while (!result.done) {
+    const [key, value] = result.value;
+    headers[key] = value;
+    result = headersIt.next();
+  }
   const cf = request.cf;
   return new Response(
     JSON.stringify({
