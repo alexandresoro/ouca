974895a71474cdaee035ad68e793921785e34eea
Alexandre Soro <code@asoro.dev>
2022-06-18 20:56:29 +0200

    Fix sentry proxy

--- functions/crash.ts
+++ functions/crash.ts
@@ -1,13 +1,18 @@
 // Sentry proxy function
-export const onRequestPost: PagesFunction = async ({ request, env }) => {
+export const onRequestPost: PagesFunction = async ({ request }) => {
   try {
     const body = await request.text();
     const pieces = body.split("\n");
 
     // DSN is in the first JSON structure
     const header = JSON.parse(pieces[0]);
+    const dsnUrl = new URL(header?.dsn);
+    const { host, pathname } = dsnUrl;
 
-    const response = await fetch(header?.dsn, {
+    // Project id is the part after the URL
+    const projectId = pathname.endsWith("/") ? pathname.slice(0, -1) : pathname;
+
+    const response = await fetch(`https://${host}/api/${projectId}/envelope/`, {
       method: "POST",
       body
     });
