beebf796f087473e755f3142a6b09c16a2267b82
Alexandre Soro <code@soro.dev>
2022-11-02 22:39:07 +0100

    build: add env var to customize API endpoint in dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,28 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import svgrPlugin from "vite-plugin-svgr";
 
-const API_SERVER_URL = "http://localhost:4000";
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-export default defineConfig({
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      "/graphql": API_SERVER_URL,
-      "/download": API_SERVER_URL
-    }
-  },
-  plugins: [
-    react(),
-    svgrPlugin({
-      svgrOptions: {
-        icon: true
+  const API_SERVER_URL = env.API_SERVER_URL ?? "http://localhost:4000";
+
+  return {
+    server: {
+      port: 3000,
+      open: true,
+      proxy: {
+        "/graphql": API_SERVER_URL,
+        "/download": API_SERVER_URL
       }
-    })
-  ]
+    },
+    plugins: [
+      react(),
+      svgrPlugin({
+        svgrOptions: {
+          icon: true
+        }
+      })
+    ]
+  };
 });
