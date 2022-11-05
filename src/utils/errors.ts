cc675e8b51cb07bf3175ace599980efea3cd8ebd
Alexandre Soro <code@soro.dev>
2022-11-05 14:50:03 +0100

    feat: add workaround to properly return applicative errors

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -8,10 +8,17 @@ type SubError = {
 export class OucaError extends Error {
   subError?: SubError;
 
+  extensions: {
+    code: string;
+    message?: string;
+    subError?: SubError;
+  };
+
   constructor(code: ErrorCode, subError?: SubError) {
     super();
     this.name = code;
     this.message = subError?.message ?? getErrorMessage(code);
     this.subError = subError;
+    this.extensions = { code, message: subError?.message ?? getErrorMessage(code), subError };
   }
 }
