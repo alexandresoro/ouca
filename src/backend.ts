306dda0d6f4631286450504008fe2e2186ee8760
Alexandre Soro <code@soro.dev>
2022-12-03 23:20:12 +0100

    feat: rework services init and extract elements from main

--- src/backend.ts
+++ src/backend.ts
@@ -1,33 +1,15 @@
-import fastifyCompress from "@fastify/compress";
-import { fastifyCookie } from "@fastify/cookie";
-import fastifyCors from "@fastify/cors";
-import fastifyMultipart from "@fastify/multipart";
-import { fastifyStatic } from "@fastify/static";
 import { fastify } from "fastify";
-import { NoSchemaIntrospectionCustomRule } from "graphql";
-import mercurius from "mercurius";
-import { randomUUID } from "node:crypto";
 import fs from "node:fs";
 import path from "node:path";
-import { pipeline } from "node:stream";
-import { promisify } from "node:util";
-import { createPool } from "slonik";
-import { createFieldNameTransformationInterceptor } from "slonik-interceptor-field-name-transformation";
-import { buildGraphQLContext } from "./graphql/graphql-context";
-import { logQueries, logResults } from "./graphql/mercurius-logger";
-import { buildResolvers } from "./graphql/resolvers";
-import { IMPORT_TYPE, type ImportType } from "./model/import-types";
-import { startImportTask } from "./services/import-manager";
+import { registerFastifyPlugins, registerFastifyRoutes } from "./fastify";
 import { buildServices } from "./services/services";
-import { createQueryLoggingInterceptor } from "./slonik/slonik-pino-interceptor";
-import { createResultParserInterceptor } from "./slonik/slonik-zod-interceptor";
 import { logger } from "./utils/logger";
 import options from "./utils/options";
-import { checkAndCreateFolders, DOWNLOAD_ENDPOINT, IMPORTS_DIR_PATH, PUBLIC_DIR_PATH } from "./utils/paths";
+import { checkAndCreateFolders } from "./utils/paths";
 
-logger.debug("Starting server");
+logger.debug("Starting app");
 
-const schema = fs.readFileSync(path.join(__dirname, "model/schema.graphql"), "utf-8").toString();
+const graphQLSchema = fs.readFileSync(path.join(__dirname, "model/schema.graphql"), "utf-8").toString();
 logger.debug("GraphQL schema has been parsed");
 
 const server = fastify({
@@ -37,107 +19,15 @@ const server = fastify({
 checkAndCreateFolders();
 
 (async () => {
-  // Database connection
-  const slonik = await createPool(options.database.url, {
-    interceptors: [
-      createFieldNameTransformationInterceptor({ format: "CAMEL_CASE" }),
-      createResultParserInterceptor(),
-      createQueryLoggingInterceptor(logger),
-    ],
-  });
+  const services = await buildServices();
+  const { slonik } = services;
+
+  logger.debug("Services initialized successfully");
   logger.debug("Connection to database successful");
 
-  // Build services
-  const services = buildServices({ logger, slonik });
+  await registerFastifyPlugins(server, services, graphQLSchema);
 
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const { tokenService } = services;
-
-  // Middlewares
-  await server.register(fastifyMultipart);
-  await server.register(fastifyCookie);
-  await server.register(fastifyCompress);
-  await server.register(fastifyCors, {
-    origin: true,
-    credentials: true,
-    maxAge: 3600,
-  });
-
-  // Static files server
-  await server.register(fastifyStatic, {
-    root: PUBLIC_DIR_PATH,
-    prefix: DOWNLOAD_ENDPOINT,
-  });
-
-  // Mercurius GraphQL adapter
-  await server.register(mercurius, {
-    schema,
-    resolvers: buildResolvers(services),
-    context: buildGraphQLContext({ tokenService }),
-    validationRules: process.env.NODE_ENV === "production" ? [NoSchemaIntrospectionCustomRule] : [],
-  });
-  server.graphql.addHook("preExecution", logQueries);
-  server.graphql.addHook("onResolution", logResults);
-
-  // Download files
-  server.get<{ Params: { id: string }; Querystring: { filename?: string } }>("/download/:id", async (req, reply) => {
-    const tokenPayload = await tokenService.validateAndExtractUserToken(req);
-    if (!tokenPayload?.sub) {
-      return reply.code(401).send();
-    }
-    return reply.download(req.params.id, req.query.filename ?? undefined);
-  });
-  server.get<{ Params: { id: string }; Querystring: { filename?: string } }>(
-    "/download/importReports/:id",
-    async (req, reply) => {
-      const tokenPayload = await tokenService.validateAndExtractUserToken(req);
-      if (!tokenPayload?.sub) {
-        return reply.code(401).send();
-      }
-
-      return reply.download(req.params.id, req.query.filename ?? undefined);
-    }
-  );
-
-  // Upload import path
-  server.post<{ Params: { entityName: string } }>("/uploads/:entityName", async (req, reply) => {
-    const tokenPayload = await tokenService.validateAndExtractUserToken(req);
-    if (!tokenPayload) {
-      return reply.code(401).send();
-    }
-    const loggedUser = tokenService.getLoggedUserInfo(tokenPayload);
-    if (!loggedUser) {
-      return reply.code(401).send();
-    }
-
-    const { params } = req;
-
-    // Check that the import is a known one
-    if (
-      !IMPORT_TYPE.find((importType) => {
-        return importType === params.entityName;
-      })
-    ) {
-      return await reply.code(404).send();
-    }
-
-    const data = await req.file();
-    if (!data) {
-      return reply.code(400).send();
-    }
-
-    const uploadId = randomUUID();
-
-    await promisify(pipeline)(data.file, fs.createWriteStream(path.join(IMPORTS_DIR_PATH, uploadId)));
-
-    startImportTask(uploadId, params.entityName as ImportType, loggedUser);
-
-    await reply.send(
-      JSON.stringify({
-        uploadId,
-      })
-    );
-  });
+  registerFastifyRoutes(server, services);
 
   await server.listen({ ...options.server });
 
--- src/services/services.ts
+++ src/services/services.ts
@@ -1,23 +1,34 @@
 import { type Logger } from "pino";
-import { type DatabasePool } from "slonik";
+import { createPool, type DatabasePool } from "slonik";
+import { createFieldNameTransformationInterceptor } from "slonik-interceptor-field-name-transformation";
 import { buildSettingsRepository } from "../repositories/settings/settings-repository";
 import { buildUserRepository } from "../repositories/user/user-repository";
+import { createQueryLoggingInterceptor } from "../slonik/slonik-pino-interceptor";
+import { createResultParserInterceptor } from "../slonik/slonik-zod-interceptor";
+import { logger } from "../utils/logger";
+import options from "../utils/options";
 import { buildSettingsService, type SettingsService } from "./settings-service";
 import { buildTokenService, type TokenService } from "./token-service";
 import { buildUserService, type UserService } from "./user-service";
 
-type Dependencies = {
+export type Services = {
   logger: Logger;
   slonik: DatabasePool;
-};
-
-export type Services = {
   settingsService: SettingsService;
   tokenService: TokenService;
   userService: UserService;
 };
 
-export const buildServices = ({ logger, slonik }: Dependencies): Services => {
+export const buildServices = async (): Promise<Services> => {
+  // Database connection
+  const slonik = await createPool(options.database.url, {
+    interceptors: [
+      createFieldNameTransformationInterceptor({ format: "CAMEL_CASE" }),
+      createResultParserInterceptor(),
+      createQueryLoggingInterceptor(logger),
+    ],
+  });
+
   const settingsRepository = buildSettingsRepository({ slonik });
   const userRepository = buildUserRepository({ slonik });
 
@@ -38,6 +49,8 @@ export const buildServices = ({ logger, slonik }: Dependencies): Services => {
   });
 
   return {
+    logger,
+    slonik,
     settingsService,
     tokenService,
     userService,
