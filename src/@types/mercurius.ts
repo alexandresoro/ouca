625c9c7a0001ede56f40959f538da1a81d3b1e01
Alexandre Soro <code@soro.dev>
2022-12-08 23:04:59 +0100

    feat: extract module augment

--- src/@types/mercurius.ts
+++ src/@types/mercurius.ts
@@ -1,6 +1,11 @@
 import { type Resolvers } from "../graphql/generated/graphql-types";
+import { type buildGraphQLContext } from "../graphql/graphql-context";
+
+type PromiseType<T> = T extends PromiseLike<infer U> ? U : T;
 
 declare module "mercurius" {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface MercuriusContext extends PromiseType<ReturnType<ReturnType<typeof buildGraphQLContext>>> {}
   // eslint-disable-next-line @typescript-eslint/no-empty-interface, @typescript-eslint/consistent-type-imports
   interface IResolvers extends Resolvers<import("mercurius").MercuriusContext> {}
 }
--- src/graphql/graphql-context.ts
+++ src/graphql/graphql-context.ts
@@ -2,13 +2,6 @@ import { type FastifyReply, type FastifyRequest } from "fastify";
 import mercurius from "mercurius";
 import { type LoggedUserInfo, type TokenService } from "../services/token-service";
 
-type PromiseType<T> = T extends PromiseLike<infer U> ? U : T;
-
-declare module "mercurius" {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface MercuriusContext extends PromiseType<ReturnType<ReturnType<typeof buildGraphQLContext>>> {}
-}
-
 export type GraphQLContext = {
   request: FastifyRequest;
   reply: FastifyReply;
