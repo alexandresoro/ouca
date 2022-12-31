e9b0a037b12a31b964097e37a3fb750698719cdf
Alexandre Soro <code@soro.dev>
2022-12-31 22:51:47 +0100

    feat: cleanup old prisma code

--- src/services/entities/donnee-service.ts
+++ src/services/entities/donnee-service.ts
@@ -8,16 +8,11 @@ import {
   type PaginatedSearchDonneesResultResultArgs,
   type SearchDonneeCriteria,
 } from "../../graphql/generated/graphql-types";
-import { type Age } from "../../repositories/age/age-repository-types";
-import { type Comportement } from "../../repositories/comportement/comportement-repository-types";
 import { type DonneeComportementRepository } from "../../repositories/donnee-comportement/donnee-comportement-repository";
 import { type DonneeMilieuRepository } from "../../repositories/donnee-milieu/donnee-milieu-repository";
 import { type DonneeRepository } from "../../repositories/donnee/donnee-repository";
 import { type Donnee } from "../../repositories/donnee/donnee-repository-types";
-import { type EstimationNombre } from "../../repositories/estimation-nombre/estimation-nombre-repository-types";
 import { type InventaireRepository } from "../../repositories/inventaire/inventaire-repository";
-import { type Milieu } from "../../repositories/milieu/milieu-repository-types";
-import { type Sexe } from "../../repositories/sexe/sexe-repository-types";
 import prisma from "../../sql/prisma";
 import { type LoggedUser } from "../../types/User";
 import { OucaError } from "../../utils/errors";
@@ -221,61 +216,6 @@ export const buildDonneeService = ({
 
 export type DonneeService = ReturnType<typeof buildDonneeService>;
 
-type DonneeWithRelations = DonneeEntity & {
-  age: Age;
-  sexe: Sexe;
-  comportements: Comportement[];
-  milieux: Milieu[];
-  estimationNombre: EstimationNombre;
-};
-
-type DonneeRelatedTablesFields = {
-  donnee_comportement: {
-    comportement: Comportement;
-  }[];
-  donnee_milieu: {
-    milieu: Milieu;
-  }[];
-};
-
-const COMMON_DONNEE_INCLUDE = {
-  age: true,
-  sexe: true,
-  estimationDistance: true,
-  estimationNombre: true,
-  donnee_comportement: {
-    select: {
-      comportement: true,
-    },
-  },
-  donnee_milieu: {
-    select: {
-      milieu: true,
-    },
-  },
-};
-
-const normalizeDonnee = <T extends DonneeRelatedTablesFields>(
-  donnee: T
-): Omit<T, "donnee_comportement" | "donnee_milieu"> & {
-  comportements: Comportement[];
-  milieux: Milieu[];
-} => {
-  const { donnee_comportement, donnee_milieu, ...restDonnee } = donnee;
-  const comportementsArray = donnee_comportement.map((donnee_comportement) => {
-    return donnee_comportement?.comportement;
-  });
-  const milieuxArray = donnee_milieu.map((donnee_milieu) => {
-    return donnee_milieu?.milieu;
-  });
-
-  return {
-    ...restDonnee,
-    comportements: comportementsArray,
-    milieux: milieuxArray,
-  };
-};
-
 export const findDonneeNavigationData = async (donneeId: number | undefined): Promise<DonneeNavigationData> => {
   const previousDonnee = await prisma.donnee.findFirst({
     select: {
@@ -376,127 +316,4 @@ export const findExistingDonnee = async (donnee: InputDonnee): Promise<DonneeEnt
       return areComportementsSameLength && areMilieuxSameLength;
     })?.[0] ?? null
   );
-};
-
-const upsertDonnee = async (args: MutationUpsertDonneeArgs): Promise<DonneeWithRelations> => {
-  const { id, data } = args;
-
-  // Check if an exact same donnee already exists or not
-  const existingDonnee = await findExistingDonnee(data);
-
-  if (existingDonnee && existingDonnee.id !== id) {
-    // The donnee already exists so we return an error
-    return Promise.reject(`Cette donnée existe déjà (ID = ${existingDonnee.id}).`);
-  } else {
-    const { comportementsIds, milieuxIds, ...restData } = data;
-
-    const comportementMap =
-      comportementsIds?.map((comportementId) => {
-        return {
-          comportement_id: comportementId,
-        };
-      }) ?? [];
-
-    const milieuMap =
-      milieuxIds?.map((milieuId) => {
-        return {
-          milieu_id: milieuId,
-        };
-      }) ?? [];
-
-    if (id) {
-      return prisma.donnee
-        .update({
-          where: { id },
-          include: COMMON_DONNEE_INCLUDE,
-          data: {
-            ...restData,
-            donnee_comportement: {
-              deleteMany: {
-                donnee_id: id,
-              },
-              create: comportementMap,
-            },
-            donnee_milieu: {
-              deleteMany: {
-                donnee_id: id,
-              },
-              create: milieuMap,
-            },
-          },
-        })
-        .then(normalizeDonnee);
-    } else {
-      return createDonnee(data);
-    }
-  }
-};
-
-const createDonnee = async (donnee: InputDonnee): Promise<DonneeWithRelations> => {
-  const { comportementsIds, milieuxIds, ...restData } = donnee;
-
-  const comportementMap =
-    comportementsIds?.map((comportementId) => {
-      return {
-        comportement_id: comportementId,
-      };
-    }) ?? [];
-
-  const milieuMap =
-    milieuxIds?.map((milieuId) => {
-      return {
-        milieu_id: milieuId,
-      };
-    }) ?? [];
-
-  return prisma.donnee
-    .create({
-      data: {
-        ...restData,
-        date_creation: new Date(),
-        donnee_comportement: {
-          create: comportementMap,
-        },
-        donnee_milieu: {
-          create: milieuMap,
-        },
-      },
-      include: COMMON_DONNEE_INCLUDE,
-    })
-    .then(normalizeDonnee);
-};
-
-export const deleteDonnee = async (donneeId: number): Promise<DonneeEntity> => {
-  // First get the corresponding inventaire_id
-  const inventaire = await prisma.donnee
-    .findUnique({
-      where: {
-        id: donneeId,
-      },
-    })
-    .inventaire();
-
-  // Delete the actual donnee
-  const deletedDonnee = await prisma.donnee.delete({
-    where: {
-      id: donneeId,
-    },
-  });
-
-  // Check how many donnees the inventaire has after the deletion
-  const nbDonneesOfInventaire = await prisma.donnee.count({
-    where: {
-      inventaireId: inventaire?.id,
-    },
-  });
-
-  if (nbDonneesOfInventaire === 0) {
-    // If the inventaire has no more donnees then we remove the inventaire
-    await prisma.inventaire.delete({
-      where: {
-        id: inventaire?.id,
-      },
-    });
-  }
-  return deletedDonnee;
 };
