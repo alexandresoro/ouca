680f0de8d9d39b6b6d1049e9c826963c34dfca75
Alexandre Soro <code@soro.dev>
2023-02-07 23:49:36 +0100

    fix: tsup config to output esm

--- packages/common/tsup.config.ts
+++ packages/common/tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src", "!src/**/*.spec.*"],
+  format: "esm",
   target: "es2022",
   sourcemap: true,
   bundle: false,
