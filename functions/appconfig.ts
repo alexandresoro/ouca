c3bd0b097babca635f570ddf5899398007f02f7d
Alexandre Soro <code@soro.dev>
2023-02-10 21:18:31 +0100

    build: fallback to default settings if host not declared

--- functions/appconfig.ts
+++ functions/appconfig.ts
@@ -5,7 +5,9 @@ type AppConfig = {
 export const onRequestGet: PagesFunction<{ API_URLS?: KVNamespace }> = async ({ request, env }) => {
   const url = new URL(request.url);
 
-  const appConfig = await env.API_URLS?.get<AppConfig>(url.hostname, "json");
+  const appConfig =
+    (await env.API_URLS?.get<AppConfig>(url.hostname, "json")) ??
+    (await env.API_URLS?.get<AppConfig>("default", "json"));
 
   const response = new Response(JSON.stringify(appConfig));
   response.headers.set("Content-Type", "application/json");
