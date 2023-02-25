0e52fe301cd1268d9918788a5e6e77bad8d46519
Alexandre Soro <code@soro.dev>
2023-02-25 23:23:20 +0100

    fix: proper typings

--- packages/frontend/src/components/utils/StyledPanelHeader.tsx
+++ packages/frontend/src/components/utils/StyledPanelHeader.tsx
@@ -1,8 +1,17 @@
-import { type FunctionComponent, type PropsWithChildren } from "react";
+import { type ComponentPropsWithoutRef, type FunctionComponent, type PropsWithChildren } from "react";
 
-const StyledPanelHeader: FunctionComponent<PropsWithChildren> = ({ children }) => {
+const StyledPanelHeader: FunctionComponent<PropsWithChildren<ComponentPropsWithoutRef<"div">>> = ({
+  children,
+  className,
+  ...restProps
+}) => {
   return (
-    <div className="box-border h-16 md:h-20 w-full flex justify-between items-center px-12 shadow shadow-gray-500/75 bg-teal-700 dark:bg-neutral-900 text-white">
+    <div
+      className={`box-border h-16 md:h-20 w-full flex items-center px-12 shadow shadow-gray-500/75 bg-teal-700 dark:bg-neutral-900 text-white ${
+        className ?? ""
+      }`}
+      {...restProps}
+    >
       {children}
     </div>
   );
