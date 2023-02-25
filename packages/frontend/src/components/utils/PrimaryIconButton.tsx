bd0d32a9ea6efd284fbf2353c1f160ba8bf12cfa
Alexandre Soro <code@soro.dev>
2023-02-25 23:11:13 +0100

    fix: typings

--- packages/frontend/src/components/utils/PrimaryIconButton.tsx
+++ packages/frontend/src/components/utils/PrimaryIconButton.tsx
@@ -1,7 +1,7 @@
-import { IconButton } from "@mui/material";
+import { IconButton, type IconButtonProps } from "@mui/material";
 import { forwardRef } from "react";
 
-const PrimaryIconButton = forwardRef<HTMLButtonElement>((props, ref) => (
+const PrimaryIconButton = forwardRef<HTMLButtonElement, IconButtonProps>((props, ref) => (
   <IconButton className="text-teal-700 dark:text-white" {...props} ref={ref} />
 ));
 
