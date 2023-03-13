13dd8ae4e5413b9f2231b8852e729b1c11e0e920
Alexandre Soro <code@soro.dev>
2023-03-14 00:31:53 +0100

    feat: improve some shadows in dark mode

--- packages/frontend/src/components/header/Header.tsx
+++ packages/frontend/src/components/header/Header.tsx
@@ -9,7 +9,7 @@ const Header: FunctionComponent = () => {
   const isSizeLarge = useIsSizeLarge();
 
   return (
-    <div className="navbar z-10 sticky bg-primary dark:bg-neutral-800 min-h-12 px-6 py-0 place-content-between shadow-md shadow-gray-700/75">
+    <div className="navbar z-10 sticky bg-primary dark:bg-neutral-800 min-h-12 px-6 py-0 place-content-between shadow-md dark:shadow shadow-gray-700/75 dark:shadow-neutral-800/50">
       <Link className="flex-shrink-0 gap-2.5 outline-none" to="/">
         <img className="-mb-3" src={Logo} height="60px" width="70px"></img>
         <h1 className="hidden text-2xl md:block text-neutral-50 font-['Yuji_Hentaigana_Akebono'] font-bold drop-shadow-[2px_2px_rgba(0,0,0,0.4)]">
