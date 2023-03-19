3514a83ab41982b76a79eefb741b0629a83b2c97
Alexandre Soro <code@soro.dev>
2023-03-19 01:36:48 +0100

    feat: remove web-vitals

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -6,7 +6,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./i18n";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
@@ -16,8 +15,3 @@ createRoot(document.getElementById("root")!).render(
     </Suspense>
   </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
