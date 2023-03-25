fdf1f7601341a1ae33c0f6939a0c24245b995088
Alexandre Soro <code@soro.dev>
2023-03-25 14:29:21 +0100

    build: remove include file for CF build to avoid warnings and uploading anything

--- packages/frontend/vite.config.cf.ts
+++ packages/frontend/vite.config.cf.ts
@@ -17,7 +17,7 @@ export default defineConfig(({ mode }) => {
       ...(env.SENTRY_URL
         ? [
             sentryVitePlugin({
-              include: "./dist",
+              include: "",
               url: env.SENTRY_URL,
               // https://github.com/getsentry/sentry-javascript-bundler-plugins/issues/182
               releaseInjectionTargets: /src\/index\.tsx$/,
