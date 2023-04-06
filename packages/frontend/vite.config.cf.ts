d7356947f3fa208811dbad280aa2dedd0eb8bbef
Alexandre Soro <code@soro.dev>
2023-04-06 22:48:17 +0200

    build: cleaner worakaround w/ new reelase of vite sentry plugin

--- packages/frontend/vite.config.cf.ts
+++ packages/frontend/vite.config.cf.ts
@@ -20,7 +20,9 @@ export default defineConfig(({ mode }) => {
               include: "",
               url: env.SENTRY_URL,
               // https://github.com/getsentry/sentry-javascript-bundler-plugins/issues/182
+              // Release 0.5.1 seems to break injection, but 0.5.0 is also broken with "auto-injection"
               releaseInjectionTargets: /src\/index\.tsx$/,
+              uploadSourceMaps: false,
               telemetry: false,
               release: env.CF_PAGES_COMMIT_SHA,
             }),
