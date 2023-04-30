61f45500857cab8b7dbbb84c50f1d0c6ad9c6c1d
Alexandre Soro <code@soro.dev>
2023-04-30 14:57:13 +0200

    fix: lint

--- packages/frontend/src/components/header/Header.tsx
+++ packages/frontend/src/components/header/Header.tsx
@@ -6,7 +6,7 @@ import HeaderSettings from "./HeaderSettings";
 
 const Header: FunctionComponent = () => {
   return (
-    <div className="navbar z-10 sticky bg-base-100 dark:bg-neutral-800 min-h-12 px-6 py-0 place-content-between dark:shadow  dark:shadow-neutral-800/50 border-b-[1px] border-base-300" >
+    <div className="navbar z-10 sticky bg-base-100 dark:bg-neutral-800 min-h-12 px-6 py-0 place-content-between dark:shadow  dark:shadow-neutral-800/50 border-b-[1px] border-base-300">
       <Link className="flex-shrink-0 gap-2.5 outline-none" to="/">
         <img className="-mb-3" src={Logo} height="60px" width="70px"></img>
         <h1 className="hidden text-[28px] md:block text-neutral font-['Carter_One'] font-bold ">
