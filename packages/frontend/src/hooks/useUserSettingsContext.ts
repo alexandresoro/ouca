3c3d85d531734a02d7bfd24181a5184023ac4da0
Alexandre Soro <code@soro.dev>
2023-05-08 18:26:17 +0200

    fix: build

--- packages/frontend/src/hooks/useUserSettingsContext.ts
+++ packages/frontend/src/hooks/useUserSettingsContext.ts
@@ -2,10 +2,10 @@ import { useContext } from "react";
 import { UserSettingsContext } from "../contexts/UserSettingsContext";
 
 export default () => {
-  const { userSettings, updateUserSettings } = useContext(UserSettingsContext);
+  const { userSettings, refetchSettings } = useContext(UserSettingsContext);
   // Here we can assert that user settings cannot be null as we do not display components inside otherwise from the context
   return {
     userSettings: userSettings as NonNullable<typeof userSettings>,
-    updateUserSettings,
+    refetchSettings,
   };
 };
