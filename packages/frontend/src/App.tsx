56a66ccaa85e7ae61c240fbba639d2423eb54795
Alexandre Soro <code@soro.dev>
2023-06-27 21:26:30 +0200

    feat: remove graphql use in frontend

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,17 +1,13 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { authExchange } from "@urql/exchange-auth";
-import { refocusExchange } from "@urql/exchange-refocus";
 import { type UserManagerSettings } from "oidc-client-ts";
 import { Suspense, lazy, useEffect, useMemo, useState, type FunctionComponent } from "react";
 import { AuthProvider } from "react-oidc-context";
 import { BrowserRouter, Navigate, Outlet, Route, Routes } from "react-router-dom";
-import { Provider as UrqlProvider, cacheExchange, createClient, fetchExchange } from "urql";
 import { AuthHandler } from "./components/AuthHandler";
 import { AppContext, DEFAULT_CONFIG } from "./contexts/AppContext";
 import { queryClient } from "./query/query-client";
 import loadAnalytics from "./services/load-analytics";
-import { getUser } from "./utils/getUser";
 import { initApp } from "./utils/init-app";
 import suspend from "./utils/suspend";
 
@@ -62,41 +58,6 @@ const App: FunctionComponent = () => {
     } satisfies UserManagerSettings;
   }, [config]);
 
-  const urqlClient = useMemo(
-    () =>
-      createClient({
-        url: `${config.apiUrl ?? ""}/graphql`,
-        exchanges: [
-          refocusExchange(),
-          cacheExchange,
-          // eslint-disable-next-line @typescript-eslint/require-await
-          authExchange(async (utils) => {
-            return {
-              addAuthToOperation(operation) {
-                const user = getUser(oidcConfig);
-                const token = user?.access_token;
-                if (!token) return operation;
-                return utils.appendHeaders(operation, {
-                  Authorization: `Bearer ${token}`,
-                });
-              },
-              didAuthError(error, _operation) {
-                return false;
-              },
-              // eslint-disable-next-line @typescript-eslint/no-empty-function
-              async refreshAuth() {},
-            };
-          }),
-          fetchExchange,
-        ],
-        requestPolicy: "cache-and-network",
-        fetchOptions: {
-          credentials: "include",
-        },
-      }),
-    [config, oidcConfig]
-  );
-
   const RouterRoutes = SentryRoutes ?? Routes;
 
   return (
@@ -109,184 +70,182 @@ const App: FunctionComponent = () => {
       >
         <QueryClientProvider client={queryClient}>
           <ReactQueryDevtools />
-          <UrqlProvider value={urqlClient}>
-            <BrowserRouter>
-              <AuthHandler>
-                <div className="bg-base-100">
-                  <Suspense fallback="">
-                    <RouterRoutes>
+          <BrowserRouter>
+            <AuthHandler>
+              <div className="bg-base-100">
+                <Suspense fallback="">
+                  <RouterRoutes>
+                    <Route
+                      path="/"
+                      element={
+                        <Suspense fallback={<></>}>
+                          <UserSettingsProvider>
+                            <Layout />
+                          </UserSettingsProvider>
+                        </Suspense>
+                      }
+                    >
+                      <Route index element={<Navigate to="/create/new" replace={true} />} />
                       <Route
-                        path="/"
+                        path="create/new"
                         element={
                           <Suspense fallback={<></>}>
-                            <UserSettingsProvider>
-                              <Layout />
-                            </UserSettingsProvider>
+                            <NewEntryPage />
                           </Suspense>
                         }
-                      >
-                        <Route index element={<Navigate to="/create/new" replace={true} />} />
+                      />
+                      <Route
+                        path="entry/:id"
+                        element={
+                          <Suspense fallback={<></>}>
+                            <ExistingEntryPage />
+                          </Suspense>
+                        }
+                      />
+                      <Route
+                        path="view"
+                        element={
+                          <Suspense fallback={<></>}>
+                            <ViewDonneesPage />
+                          </Suspense>
+                        }
+                      />
+                      <Route path="manage" element={<Outlet />}>
                         <Route
-                          path="create/new"
+                          path="observateur/*"
                           element={
                             <Suspense fallback={<></>}>
-                              <NewEntryPage />
+                              <ObservateurManage />
                             </Suspense>
                           }
                         />
                         <Route
-                          path="entry/:id"
+                          path="departement/*"
                           element={
                             <Suspense fallback={<></>}>
-                              <ExistingEntryPage />
+                              <DepartementManage />
                             </Suspense>
                           }
                         />
                         <Route
-                          path="view"
+                          path="commune/*"
                           element={
                             <Suspense fallback={<></>}>
-                              <ViewDonneesPage />
-                            </Suspense>
-                          }
-                        />
-                        <Route path="manage" element={<Outlet />}>
-                          <Route
-                            path="observateur/*"
-                            element={
-                              <Suspense fallback={<></>}>
-                                <ObservateurManage />
-                              </Suspense>
-                            }
-                          />
-                          <Route
-                            path="departement/*"
-                            element={
-                              <Suspense fallback={<></>}>
-                                <DepartementManage />
-                              </Suspense>
-                            }
-                          />
-                          <Route
-                            path="commune/*"
-                            element={
-                              <Suspense fallback={<></>}>
-                                <CommuneManage />
-                              </Suspense>
-                            }
-                          />
-                          <Route
-                            path="lieudit/*"
-                            element={
-                              <Suspense fallback={<></>}>
-                                <LieuDitManage />
-                              </Suspense>
-                            }
-                          />
-                          <Route
-                            path="meteo/*"
-                            element={
-                              <Suspense fallback={<></>}>
-                                <MeteoManage />
-                              </Suspense>
-                            }
-                          />
-                          <Route
-                            path="classe/*"
-                            element={
-                              <Suspense fallback={<></>}>
-                                <ClasseManage />
-                              </Suspense>
-                            }
-                          />
-                          <Route
-                            path="espece/*"
-                            element={
-                              <Suspense fallback={<></>}>
-                                <EspeceManage />
-                              </Suspense>
-                            }
-                          />
-                          <Route
-                            path="sexe/*"
-                            element={
-                              <Suspense fallback={<></>}>
-                                <SexeManage />
-                              </Suspense>
-                            }
-                          />
-                          <Route
-                            path="age/*"
-                            element={
-                              <Suspense fallback={<></>}>
-                                <AgeManage />
-                              </Suspense>
-                            }
-                          />
-                          <Route
-                            path="estimation-nombre/*"
-                            element={
-                              <Suspense fallback={<></>}>
-                                <EstimationNombreManage />
-                              </Suspense>
-                            }
-                          />
-                          <Route
-                            path="estimation-distance/*"
-                            element={
-                              <Suspense fallback={<></>}>
-                                <EstimationDistanceManage />
-                              </Suspense>
-                            }
-                          />
-                          <Route
-                            path="comportement/*"
-                            element={
-                              <Suspense fallback={<></>}>
-                                <ComportementManage />
-                              </Suspense>
-                            }
-                          />
-                          <Route
-                            path="milieu/*"
-                            element={
-                              <Suspense fallback={<></>}>
-                                <MilieuManage />
-                              </Suspense>
-                            }
-                          />
-                        </Route>
-                        <Route
-                          path="profile"
-                          element={
-                            <Suspense fallback={<></>}>
-                              <UserProfilePage />
+                              <CommuneManage />
                             </Suspense>
                           }
                         />
                         <Route
-                          path="settings"
+                          path="lieudit/*"
                           element={
                             <Suspense fallback={<></>}>
-                              <SettingsPage />
+                              <LieuDitManage />
+                            </Suspense>
+                          }
+                        />
+                        <Route
+                          path="meteo/*"
+                          element={
+                            <Suspense fallback={<></>}>
+                              <MeteoManage />
+                            </Suspense>
+                          }
+                        />
+                        <Route
+                          path="classe/*"
+                          element={
+                            <Suspense fallback={<></>}>
+                              <ClasseManage />
+                            </Suspense>
+                          }
+                        />
+                        <Route
+                          path="espece/*"
+                          element={
+                            <Suspense fallback={<></>}>
+                              <EspeceManage />
+                            </Suspense>
+                          }
+                        />
+                        <Route
+                          path="sexe/*"
+                          element={
+                            <Suspense fallback={<></>}>
+                              <SexeManage />
+                            </Suspense>
+                          }
+                        />
+                        <Route
+                          path="age/*"
+                          element={
+                            <Suspense fallback={<></>}>
+                              <AgeManage />
+                            </Suspense>
+                          }
+                        />
+                        <Route
+                          path="estimation-nombre/*"
+                          element={
+                            <Suspense fallback={<></>}>
+                              <EstimationNombreManage />
+                            </Suspense>
+                          }
+                        />
+                        <Route
+                          path="estimation-distance/*"
+                          element={
+                            <Suspense fallback={<></>}>
+                              <EstimationDistanceManage />
+                            </Suspense>
+                          }
+                        />
+                        <Route
+                          path="comportement/*"
+                          element={
+                            <Suspense fallback={<></>}>
+                              <ComportementManage />
+                            </Suspense>
+                          }
+                        />
+                        <Route
+                          path="milieu/*"
+                          element={
+                            <Suspense fallback={<></>}>
+                              <MilieuManage />
                             </Suspense>
                           }
                         />
                       </Route>
                       <Route
-                        path="new-account"
+                        path="profile"
                         element={
                           <Suspense fallback={<></>}>
-                            <NewAccount />
+                            <UserProfilePage />
                           </Suspense>
                         }
                       />
-                    </RouterRoutes>
-                  </Suspense>
-                </div>
-              </AuthHandler>
-            </BrowserRouter>
-          </UrqlProvider>
+                      <Route
+                        path="settings"
+                        element={
+                          <Suspense fallback={<></>}>
+                            <SettingsPage />
+                          </Suspense>
+                        }
+                      />
+                    </Route>
+                    <Route
+                      path="new-account"
+                      element={
+                        <Suspense fallback={<></>}>
+                          <NewAccount />
+                        </Suspense>
+                      }
+                    />
+                  </RouterRoutes>
+                </Suspense>
+              </div>
+            </AuthHandler>
+          </BrowserRouter>
         </QueryClientProvider>
       </AuthProvider>
     </AppContext.Provider>
