03d21039f56bdc69c2b38201f3e2cf6bd8951df8
Alexandre Soro <code@soro.dev>
2023-07-22 17:44:28 +0200

    feat: remove signature overload from useApiMutation

--- packages/frontend/src/hooks/api/useApiMutation.ts
+++ packages/frontend/src/hooks/api/useApiMutation.ts
@@ -1,4 +1,4 @@
-import { useMutation, type UseMutationOptions, type UseMutationResult } from "@tanstack/react-query";
+import { useMutation, type MutationFunction, type UseMutationOptions } from "@tanstack/react-query";
 import { useAuth } from "react-oidc-context";
 import { type z } from "zod";
 import useAppContext from "../useAppContext";
@@ -22,67 +22,61 @@ type MutationParams<R> = MutationParamsSchema<R> | MutationParamsResponseHandler
 
 type MutationVariables = { path?: string; body?: Record<string, unknown> };
 
-function useApiMutation<TData, TVariables extends MutationVariables>(
-  { path, method }: MutationParamsSchema<TData>,
-  mutationOptions?: Omit<UseMutationOptions<TData, FetchError, TVariables>, "mutationFn">
-): UseMutationResult<TData, FetchError, TVariables>;
-function useApiMutation<TData, TVariables extends MutationVariables>(
-  { path, method, responseHandler }: MutationParamsResponseHandler<TData>,
-  mutationOptions?: Omit<UseMutationOptions<TData, unknown, TVariables>, "mutationFn">
-): UseMutationResult<TData, unknown, TVariables>;
-function useApiMutation<TData, TError, TVariables extends MutationVariables>(
+export const useApiMutation = <TData, TVariables extends MutationVariables, TError extends FetchError = FetchError>(
   { path: pathFromOptions, method, responseHandler, schema }: MutationParams<TData>,
-  mutationOptions?: Omit<UseMutationOptions<unknown, TError, TVariables>, "mutationFn">
-): UseMutationResult<unknown, TError, TVariables> {
+  mutationOptions?: Omit<UseMutationOptions<TData, TError, TVariables>, "mutationFn">
+) => {
   const { user } = useAuth();
   const { apiUrl } = useAppContext();
 
   const accessToken = user?.access_token;
 
-  return useMutation<unknown, TError, TVariables>({
+  const mutationFn: MutationFunction<TData, TVariables> = async (variables) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { path: pathFromMutate, body, ...restVariables } = variables;
+
+    const path = pathFromMutate ?? pathFromOptions;
+
+    // TODO improve handling
+    if (!path) {
+      throw new Error("missing path param");
+    }
+
+    const response = await fetch(`${apiUrl}/api/v1${path}`, {
+      method,
+      headers: {
+        ...(accessToken
+          ? {
+              Authorization: `Bearer ${accessToken}`,
+            }
+          : {}),
+      },
+      body: body ? JSON.stringify(body) : null,
+    });
+
+    if (typeof responseHandler === "function") {
+      return responseHandler(response);
+    }
+
+    if (!response.ok) {
+      return Promise.reject({
+        status: response.status,
+        statusText: response.statusText,
+      } satisfies FetchError);
+    }
+    const jsonResponse = (await response.json()) as TData;
+    if (schema) {
+      // If response schema is provided, use it
+      return schema.parse(jsonResponse);
+    } else {
+      return jsonResponse;
+    }
+  };
+
+  return useMutation({
     ...mutationOptions,
-    mutationFn: async (variables) => {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const { path: pathFromMutate, body, ...restVariables } = variables;
-
-      const path = pathFromMutate ?? pathFromOptions;
-
-      // TODO improve handling
-      if (!path) {
-        throw new Error("missing path param");
-      }
-
-      const response = await fetch(`${apiUrl}/api/v1${path}`, {
-        method,
-        headers: {
-          ...(accessToken
-            ? {
-                Authorization: `Bearer ${accessToken}`,
-              }
-            : {}),
-        },
-        body: body ? JSON.stringify(body) : null,
-      });
-
-      if (typeof responseHandler === "function") {
-        return responseHandler(response);
-      }
-
-      if (!response.ok) {
-        return Promise.reject({
-          status: response.status,
-          statusText: response.statusText,
-        } satisfies FetchError);
-      }
-      const jsonResponse = (await response.json()) as unknown;
-      if (schema) {
-        // If response schema is provided, use it
-        return schema.parse(jsonResponse);
-      } else {
-        return jsonResponse;
-      }
-    },
+    mutationFn,
   });
-}
+};
 
 export default useApiMutation;
