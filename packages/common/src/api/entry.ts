90c2465badd77e9902da7c5f4f9b78d23cdc7c16
Alexandre Soro <code@soro.dev>
2023-08-07 22:47:42 +0200

    fix(entry): prevent duplicates in behaviors and environments (#1192)

--- packages/common/src/api/entry.ts
+++ packages/common/src/api/entry.ts
@@ -52,20 +52,25 @@ export const getEntriesExtendedResponse = getPaginatedResponseSchema(entryExtend
  * `PUT` `/entry/:id` Update of entry
  * `POST` `/entry` Create new entry
  */
-export const upsertEntryInput = z.object({
-  inventoryId: z.string().trim().min(1),
-  speciesId: z.string().trim().min(1),
-  sexId: z.string().trim().min(1),
-  ageId: z.string().trim().min(1),
-  numberEstimateId: z.string().trim().min(1),
-  number: z.number().int().positive().nullable(),
-  distanceEstimateId: z.string().trim().min(1).nullable(),
-  distance: z.number().int().nonnegative().nullable(),
-  regroupment: z.number().int().positive().nullable(),
-  comment: z.string().trim().min(1).nullable(),
-  behaviorIds: z.array(z.string().trim().min(1)),
-  environmentIds: z.array(z.string().trim().min(1)),
-});
+export const upsertEntryInput = z
+  .object({
+    inventoryId: z.string().trim().min(1),
+    speciesId: z.string().trim().min(1),
+    sexId: z.string().trim().min(1),
+    ageId: z.string().trim().min(1),
+    numberEstimateId: z.string().trim().min(1),
+    number: z.number().int().positive().nullable(),
+    distanceEstimateId: z.string().trim().min(1).nullable(),
+    distance: z.number().int().nonnegative().nullable(),
+    regroupment: z.number().int().positive().nullable(),
+    comment: z.string().trim().min(1).nullable(),
+    behaviorIds: z.array(z.string().trim().min(1)),
+    environmentIds: z.array(z.string().trim().min(1)),
+  })
+  .refine(({ behaviorIds, environmentIds }) => {
+    // Prevent duplicates in behavior/environment
+    return new Set(behaviorIds).size === behaviorIds.length && new Set(environmentIds).size === environmentIds.length;
+  });
 
 export type UpsertEntryInput = z.infer<typeof upsertEntryInput>;
 
