161f4bac47b8d05cc1f4a0eff3a19c6d57ffe99e
Alexandre Soro <code@soro.dev>
2023-10-28 20:02:23 +0200

    fix: worakaround to coerce to bigint to numbers following slonik upgrade

--- packages/backend/src/repositories/common.ts
+++ packages/backend/src/repositories/common.ts
@@ -5,5 +5,7 @@ const SORT_ORDER = ["asc", "desc", null] as const;
 export type SortOrder = typeof SORT_ORDER[number];
 
 export const countSchema = z.object({
-  count: z.number(),
+  // TODO: avoid coercion and handle gracefully BigInt
+  // /!\ They are not serializable
+  count: z.bigint().transform((v) => Number(v)),
 });
