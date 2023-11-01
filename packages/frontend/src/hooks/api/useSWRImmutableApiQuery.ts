a0b3aa0fbb391366fad64821d47dab96165c2524
Alexandre Soro <code@soro.dev>
2023-11-01 10:54:41 +0100

    feat(api): add immutable swr query

---
+++ packages/frontend/src/hooks/api/useSWRImmutableApiQuery.ts
@@ -0,0 +1,32 @@
+import useAppContext from "@hooks/useAppContext";
+import fetchApi from "@utils/fetch-api";
+import { toUrlSearchParams } from "@utils/url/url-search-params";
+import { useAuth } from "react-oidc-context";
+import { type SWRConfiguration } from "swr";
+import useSWRImmutable from "swr/immutable";
+import { type z } from "zod";
+
+type UseSWRApiQueryParams<T = unknown> = {
+  queryParams?: Record<string, string | number | string[] | number[] | boolean | undefined>;
+  schema?: z.ZodType<T>;
+  paused?: boolean;
+};
+
+const useSWRImmutableApiQuery = <T = unknown>(
+  path: string,
+  { queryParams, paused, schema }: UseSWRApiQueryParams<T> = {},
+  swrOptions?: Omit<SWRConfiguration, "fetcher">
+) => {
+  const { user } = useAuth();
+  const { apiUrl } = useAppContext();
+
+  const accessToken = user?.access_token;
+
+  const queryString = toUrlSearchParams(queryParams).toString();
+
+  const queryUrl = `${apiUrl}/api/v1${path}${queryString.length ? `?${queryString}` : ""}`;
+
+  return useSWRImmutable<T>(!paused ? { url: queryUrl, token: accessToken, schema } : null, fetchApi, swrOptions);
+};
+
+export default useSWRImmutableApiQuery;
