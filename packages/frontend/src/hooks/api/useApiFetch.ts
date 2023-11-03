03fc027835900ef6c79babe227c642b9a4f594f4
Alexandre Soro <code@soro.dev>
2023-11-03 13:46:45 +0100

    feat: allow useApiFetch to provide path directly

--- packages/frontend/src/hooks/api/useApiFetch.ts
+++ packages/frontend/src/hooks/api/useApiFetch.ts
@@ -5,10 +5,25 @@ import { useCallback } from "react";
 import { useAuth } from "react-oidc-context";
 import { type z } from "zod";
 
-const useApiFetch = <SType>({
+type UseApiFetchParams<T> = { path?: string; method?: string; schema?: z.ZodType<T> };
+
+type UseApiFetchCallParams = {
+  path?: string;
+  body?: Record<string, unknown>;
+  queryParams?: Record<string, string | number | boolean | undefined>;
+};
+
+function useApiFetch<T>({
+  path,
   method,
   schema,
-}: { method?: string; schema?: z.ZodType<SType> }): (({
+}: { path: string; method?: string; schema?: z.ZodType<T> }): (
+  options?: Omit<UseApiFetchCallParams, "path">
+) => Promise<T>;
+function useApiFetch<T>({
+  method,
+  schema,
+}: Omit<UseApiFetchParams<T>, "path">): ({
   path,
   body,
   queryParams,
@@ -16,23 +31,27 @@ const useApiFetch = <SType>({
   path: string;
   body?: Record<string, unknown>;
   queryParams?: Record<string, string | number | boolean | undefined>;
-}) => Promise<SType>) => {
+}) => Promise<T>;
+function useApiFetch<T>({
+  path: pathFromSignature,
+  method,
+  schema,
+}: UseApiFetchParams<T>): (params?: UseApiFetchCallParams) => Promise<T> {
   const { user } = useAuth();
   const { apiUrl } = useAppContext();
 
   const accessToken = user?.access_token;
 
   return useCallback(
-    async ({
-      path,
-      body,
-      queryParams,
-    }: {
-      path: string;
-      body?: Record<string, unknown>;
-      queryParams?: Record<string, string | number | boolean | undefined>;
-    }) => {
+    async (params?: UseApiFetchCallParams) => {
+      const { path: pathFromCall, body, queryParams } = params ?? {};
+
       const queryString = toUrlSearchParams(queryParams).toString();
+
+      const path = pathFromSignature ?? pathFromCall;
+      if (!path) {
+        throw new Error("Missing path in query");
+      }
 
       return fetchApi({
         url: `${apiUrl}/api/v1${path}${queryString.length ? `?${queryString}` : ""}`,
@@ -42,8 +61,8 @@ const useApiFetch = <SType>({
         schema,
       });
     },
-    [apiUrl, accessToken, method, schema]
+    [apiUrl, accessToken, pathFromSignature, method, schema]
   );
-};
+}
 
 export default useApiFetch;
