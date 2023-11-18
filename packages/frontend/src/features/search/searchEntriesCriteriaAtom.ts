a864765847bafd83576d3ebdfb8da14f11338166
Alexandre Soro <code@soro.dev>
2023-11-18 13:44:37 +0100

    chore(search): add atom toi filter by breeding status

--- packages/frontend/src/features/search/searchEntriesCriteriaAtom.ts
+++ packages/frontend/src/features/search/searchEntriesCriteriaAtom.ts
@@ -3,6 +3,7 @@ import { type Behavior } from "@ou-ca/common/entities/behavior";
 import { type Department } from "@ou-ca/common/entities/department";
 import { type Species } from "@ou-ca/common/entities/species";
 import { type Town } from "@ou-ca/common/entities/town";
+import { type NicheurCode } from "@ou-ca/common/types/nicheur.model";
 import { atom } from "jotai";
 
 export const searchEntriesFilterDepartmentsAtom = atom<Department[]>([]);
@@ -13,15 +14,19 @@ export const searchEntriesFilterSpeciesAtom = atom<Species[]>([]);
 
 export const searchEntriesFilterBehaviorsAtom = atom<Behavior[]>([]);
 
+export const searchEntriesFilterBreedersAtom = atom<NicheurCode[]>([]);
+
 export const searchEntriesCriteriaAtom = atom((get) => {
   const departmentIds = get(searchEntriesFilterDepartmentsAtom).map(({ id }) => id);
   const townIds = get(searchEntriesFilterTownsAtom).map(({ id }) => id);
   const speciesIds = get(searchEntriesFilterSpeciesAtom).map(({ id }) => id);
   const behaviorIds = get(searchEntriesFilterBehaviorsAtom).map(({ id }) => id);
+  const breeders = get(searchEntriesFilterBreedersAtom);
   return {
     departmentIds,
     townIds,
     speciesIds,
     behaviorIds,
+    breeders: breeders.length ? breeders : undefined,
   } satisfies SearchCriteriaParams;
 });
