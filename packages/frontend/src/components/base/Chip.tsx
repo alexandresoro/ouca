823f5a0a49c9f3f830d1875ba6fd721f14f99b21
Alexandre Soro <code@soro.dev>
2023-11-19 01:36:33 +0100

    feat(ui): add chip component

---
+++ packages/frontend/src/components/base/Chip.tsx
@@ -0,0 +1,24 @@
+import { X } from "@styled-icons/boxicons-regular";
+import { type FunctionComponent } from "react";
+
+type ChipProps = {
+  content: string;
+  onDelete?: () => void;
+};
+
+const Chip: FunctionComponent<ChipProps> = ({ content, onDelete }) => {
+  return (
+    <div className="inline-flex items-center gap-1.5 px-2 py-1 rounded-full bg-secondary bg-opacity-30 border border-secondary border-opacity-30">
+      <div className="text-sm">{content}</div>
+      <button
+        type="button"
+        className="btn btn-xs btn-circle btn-ghost border border-gray-600 border-opacity-25 min-h-[1.25rem] h-5 w-5"
+        onClick={onDelete}
+      >
+        <X className="h-4" />
+      </button>
+    </div>
+  );
+};
+
+export default Chip;
