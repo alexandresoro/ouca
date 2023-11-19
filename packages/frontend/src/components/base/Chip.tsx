c8e3b6f4f57ff367da8167d730ea368089b50daa
Alexandre Soro <code@soro.dev>
2023-11-19 23:24:49 +0100

    fix(chip): use text ellipsis to avoid multiline on long text

--- packages/frontend/src/components/base/Chip.tsx
+++ packages/frontend/src/components/base/Chip.tsx
@@ -8,8 +8,8 @@ type ChipProps = {
 
 const Chip: FunctionComponent<ChipProps> = ({ content, onDelete }) => {
   return (
-    <div className="inline-flex items-center gap-1.5 px-2 py-1 rounded-full bg-secondary bg-opacity-30 border border-secondary border-opacity-30">
-      <div className="text-sm">{content}</div>
+    <div className="inline-flex min-w-0 items-center gap-1.5 px-2 py-1 rounded-full bg-secondary bg-opacity-30 border border-secondary border-opacity-30">
+      <div className="text-sm overflow-hidden text-ellipsis whitespace-nowrap">{content}</div>
       <button
         type="button"
         className="btn btn-xs btn-circle btn-ghost border border-gray-600 border-opacity-25 min-h-[1.25rem] h-5 w-5"
