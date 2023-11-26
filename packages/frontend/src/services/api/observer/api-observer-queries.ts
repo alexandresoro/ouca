c3854a5c92c6a52ec13d929389d2a297cc3d6390
Alexandre Soro <code@soro.dev>
2023-11-26 21:46:26 +0100

    feat(observer): add swr hooks

---
+++ packages/frontend/src/services/api/observer/api-observer-queries.ts
@@ -0,0 +1,43 @@
+import { deleteObserverResponse, getObserverResponse } from "@ou-ca/common/api/observer";
+import useApiMutation from "@services/api/useApiMutation";
+import useApiQuery from "@services/api/useApiQuery";
+import { type SWRMutationConfiguration } from "swr/mutation";
+import { type z } from "zod";
+
+export const useApiObserverQuery = (id: string) => {
+  return useApiQuery(`/observers/${id}`, {
+    schema: getObserverResponse,
+  });
+};
+
+export const useApiObserverUpdate = (
+  id: string | null,
+  swrOptions?: SWRMutationConfiguration<z.infer<typeof getObserverResponse>, unknown>
+) => {
+  return useApiMutation(
+    id ? `/observers/${id}` : null,
+    {
+      method: "PUT",
+      schema: getObserverResponse,
+    },
+    {
+      revalidate: false,
+      populateCache: true,
+      ...swrOptions,
+    }
+  );
+};
+
+export const useApiObserverDelete = (
+  id: string | null,
+  swrOptions?: SWRMutationConfiguration<z.infer<typeof deleteObserverResponse>, unknown>
+) => {
+  return useApiMutation(
+    id ? `/observers/${id}` : null,
+    {
+      method: "DELETE",
+      schema: deleteObserverResponse,
+    },
+    swrOptions
+  );
+};
