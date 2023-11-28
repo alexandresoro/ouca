5110cfbc43766c23e61e5587fec3490b4b742032
Alexandre Soro <code@soro.dev>
2023-11-28 23:22:18 +0100

    chore: add some links to pg errors

--- packages/backend/src/infrastructure/kysely/database-errors.ts
+++ packages/backend/src/infrastructure/kysely/database-errors.ts
@@ -1,6 +1,10 @@
 import { type EntityFailureReason } from "@domain/shared/failure-reason.js";
 import pg from "pg";
 
+// Sources:
+// https://www.postgresql.org/docs/current/errcodes-appendix.html
+// https://github.com/brianc/node-postgres/blob/9c3ecdca6953b8cde01fcb6ab6042e8274dc2e9b/packages/pg-protocol/src/parser.ts#L369
+
 const isDatabaseError = (error: unknown): error is pg.DatabaseError => {
   return error instanceof pg.DatabaseError;
 };
