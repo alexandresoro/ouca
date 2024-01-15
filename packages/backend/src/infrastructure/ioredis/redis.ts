de4d57ac2cbdf7c93cca84af80eb00ab76ecb607
Alexandre Soro <code@soro.dev>
2024-01-15 22:42:28 +0100

    fix: add redis options

--- packages/backend/src/infrastructure/ioredis/redis.ts
+++ packages/backend/src/infrastructure/ioredis/redis.ts
@@ -1,4 +1,8 @@
 import { redisConfig } from "@infrastructure/config/redis-config.js";
 import { Redis } from "ioredis";
 
-export const redis = new Redis(redisConfig.url);
+export const redis = new Redis(redisConfig.url, {
+  // https://github.com/OptimalBits/bull/commit/3ade8e6727d7b906a30b09bccb6dc10d76ed1b5f
+  maxRetriesPerRequest: null,
+  enableReadyCheck: false,
+});
