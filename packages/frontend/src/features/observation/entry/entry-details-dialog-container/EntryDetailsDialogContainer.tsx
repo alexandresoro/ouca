413d6802c32fce050306f55dd7ed2cf7271a29a0
Camille Carrier <camille.carrier@outlook.com>
2024-04-10 13:22:42 +0200

    feat: improve entry ID display

--- packages/frontend/src/features/observation/entry/entry-details-dialog-container/EntryDetailsDialogContainer.tsx
+++ packages/frontend/src/features/observation/entry/entry-details-dialog-container/EntryDetailsDialogContainer.tsx
@@ -1,6 +1,5 @@
 import { Dialog } from "@headlessui/react";
 import type { EntryExtended } from "@ou-ca/common/api/entities/entry";
-import { Link } from "@styled-icons/boxicons-regular";
 import type { FunctionComponent } from "react";
 import { useTranslation } from "react-i18next";
 import InventorySummaryPanel from "../../inventory/inventory-summary-panel/InventorySummaryPanel";
@@ -22,18 +21,10 @@ const EntryDetailsDialogContainer: FunctionComponent<EntryDetailsDialogContainer
   return (
     <Dialog className={`modal ${open ? "modal-open" : ""}`} open={open} onClose={onClose}>
       <Dialog.Panel className="modal-box max-w-7xl">
-        <Dialog.Title className="text-2xl font-semibold py-4 first-letter:uppercase">{`${t("observation")} - ${
-          entry.species.nomFrancais
-        }`}</Dialog.Title>
-        <div className="mb-2 text-[13px]">
-          {t("observationDetails.mainSubtitle", {
-            owner: entry.inventory.observer.libelle,
-            creationDate: new Intl.DateTimeFormat().format(new Date(entry.inventory.date)),
-            updatedDate: new Intl.DateTimeFormat().format(new Date(entry.inventory.date)),
-            inventoryId: entry.inventory.id,
-            observationId: entry.id,
-          })}
-        </div>
+        <Dialog.Title className="text-2xl font-semibold py-4 first-letter:uppercase flex justify-between">
+          <span>{entry.species.nomFrancais}</span>
+          <span className="badge badge-outline text-secondary font-normal">ID {entry.id}</span>
+        </Dialog.Title>
         <div className="flex gap-8">
           <div className="basis-2/5">
             <h3 className="text-xl font-normal py-4 first-letter:uppercase">
@@ -42,21 +33,7 @@ const EntryDetailsDialogContainer: FunctionComponent<EntryDetailsDialogContainer
             <InventorySummaryPanel inventory={entry.inventory} />
           </div>
           <div className="basis-3/5">
-            <h3 className="flex justify-between py-4">
-              <span className="text-xl font-normal first-letter:uppercase">{t("observation")}</span>
-              {entry.regroupment ? (
-                <>
-                  <span className="badge badge-primary badge-outline badge-lg">
-                    <Link className="h-4 pr-1.5" />
-                    {t("observationDetails.group", {
-                      group: entry.regroupment,
-                    })}
-                  </span>
-                </>
-              ) : (
-                <></>
-              )}
-            </h3>
+            <h3 className="py-4 text-xl font-normal first-letter:uppercase">{t("observation")}</h3>
             <EntrySummaryPanel entry={entry} />
           </div>
         </div>
