188edd124155512e79be05993a733404907885cc
Alexandre Soro <code@soro.dev>
2024-04-17 23:18:24 +0200

    feat(search): use simple entry for row

--- packages/frontend/src/features/search/search-entries-table/SearchEntriesTableRow.tsx
+++ packages/frontend/src/features/search/search-entries-table/SearchEntriesTableRow.tsx
@@ -1,5 +1,7 @@
 import IconButton from "@components/base/IconButton";
-import type { EntryExtended } from "@ou-ca/common/api/entities/entry";
+import type { Entry } from "@ou-ca/common/api/entities/entry";
+import { useApiInventoryQuery } from "@services/api/inventory/api-inventory-queries";
+import { useApiSpeciesQuery } from "@services/api/species/api-species-queries";
 import { Detail, EditAlt, Trash } from "@styled-icons/boxicons-regular";
 import { Binoculars } from "@styled-icons/boxicons-solid";
 import type { FunctionComponent } from "react";
@@ -7,7 +9,7 @@ import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
 type SearchEntriesTableRowProps = {
-  donnee: EntryExtended;
+  donnee: Entry;
   onViewAction: () => void;
   onEditAction?: () => void;
   onDeleteAction: () => void;
@@ -18,21 +20,23 @@ const SearchEntriesTableRow: FunctionComponent<SearchEntriesTableRowProps> = (pr
 
   const { t } = useTranslation();
 
+  const { data: species } = useApiSpeciesQuery(donnee.species.id);
+  const { data: inventory } = useApiInventoryQuery(donnee.inventoryId);
+
   return (
     <>
       <tr className="table-hover">
-        <td>{donnee.species.nomFrancais}</td>
+        <td>{species?.nomFrancais}</td>
         <td>{donnee.number}</td>
         <td>
-          {donnee.inventory.locality.townName} ({donnee.inventory.locality.departmentCode}),{" "}
-          {donnee.inventory.locality.nom}
+          {inventory?.locality.townName} ({inventory?.locality.departmentCode}), {inventory?.locality.nom}
         </td>
-        <td>{new Intl.DateTimeFormat().format(new Date(donnee?.inventory.date))}</td>
+        <td>{inventory?.date ? new Intl.DateTimeFormat().format(new Date(inventory.date)) : ""}</td>
         <td align="right" className="w-40 flex gap-1">
           <div className="tooltip tooltip-bottom" data-tip={t("goToInventory")}>
             <Link
               className="btn btn-circle btn-sm btn-ghost text-primary"
-              to={`/inventory/${donnee.inventory.id}`}
+              to={`/inventory/${donnee.inventoryId}`}
               aria-label={t("goToInventory")}
             >
               <Binoculars className="h-5" />
