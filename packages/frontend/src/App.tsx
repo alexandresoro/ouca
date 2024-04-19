6e771eef75fb5f3f8d856ff92ba6d028f2f17d8a
Alexandre Soro <code@soro.dev>
2024-04-19 23:34:49 +0200

    chore(deps): remove tanstack query devtools

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { oidcConfigAtom } from "@services/auth/oidc-config";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useAtomValue } from "jotai";
 import { addProtocol, removeProtocol } from "maplibre-gl";
 import { Protocol } from "pmtiles";
@@ -34,7 +33,6 @@ const App: FunctionComponent<AppProps> = ({ router }) => {
         }}
       >
         <QueryClientProvider client={queryClient}>
-          <ReactQueryDevtools />
           <RouterProvider router={router} />
         </QueryClientProvider>
       </AuthProvider>
