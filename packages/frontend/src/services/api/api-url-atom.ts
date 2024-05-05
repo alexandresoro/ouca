f540ab5e1c8ac4f366f1f1cbf65435c7222256ef
Alexandre Soro <code@soro.dev>
2024-05-05 21:02:48 +0200

    feat(api): expose atom for api url path

---
+++ packages/frontend/src/services/api/api-url-atom.ts
@@ -0,0 +1,11 @@
+import { configAtom } from "@services/config/config";
+import { atom } from "jotai";
+
+const API_PATH = "/api/v1";
+
+export const apiUrlAtom = atom((get) => {
+  const config = get(configAtom);
+  const apiUrl = config.apiUrl ?? "";
+
+  return `${apiUrl}${API_PATH}`;
+});
