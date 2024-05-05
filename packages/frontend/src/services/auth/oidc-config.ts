d133f5f0962619d41a2114e7fd4b2f1b84cfcf98
Alexandre Soro <code@soro.dev>
2024-05-05 20:17:53 +0200

    feat(auth): rework oidc user atoms

--- packages/frontend/src/services/auth/oidc-config.ts
+++ packages/frontend/src/services/auth/oidc-config.ts
@@ -1,6 +1,8 @@
 import { configAtom } from "@services/config/config";
 import { atom } from "jotai";
-import { type UserManagerSettings, WebStorageStateStore } from "oidc-client-ts";
+import { User, type UserManagerSettings, WebStorageStateStore } from "oidc-client-ts";
+
+const store = window.localStorage;
 
 export const oidcConfigAtom = atom<UserManagerSettings>((get) => {
   const oidcConfig = get(configAtom).oidc;
@@ -8,6 +10,18 @@ export const oidcConfigAtom = atom<UserManagerSettings>((get) => {
     ...oidcConfig,
     redirect_uri: `${window.location.protocol}//${window.location.host}/`,
     scope: "openid email profile offline_access",
-    userStore: new WebStorageStateStore({ store: window.localStorage }),
+    userStore: new WebStorageStateStore({ store }),
   };
+});
+
+export const oidcUserAtom = atom<User | null>((get) => {
+  const oidcConfig = get(configAtom).oidc;
+
+  const oidcStorage = store.getItem(`oidc.user:${oidcConfig.authority}:${oidcConfig.client_id}`);
+
+  if (!oidcStorage) {
+    return null;
+  }
+
+  return User.fromStorageString(oidcStorage);
 });
