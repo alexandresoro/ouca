f7cee83be28153da40058b582caf5bc26b10f026
Alexandre Soro <code@soro.dev>
2024-12-23 00:36:34 +0100

    feat(maps): remove ign classique maps

--- packages/frontend/src/features/maps/map-style-providers.ts
+++ packages/frontend/src/features/maps/map-style-providers.ts
@@ -29,7 +29,7 @@ const osmProtoMapsAtom = atom<MapStyle>((get) => {
   };
 });
 
-export type MapProvider = "osm" | "ign" | "ignClassique" | "ignSatellite";
+export type MapProvider = "osm" | "ign" | "ignSatellite";
 
 export const mapStyleProvidersAtom = atom((get) => {
   const { staticAssetsUrl } = get(configAtom);
@@ -37,7 +37,6 @@ export const mapStyleProvidersAtom = atom((get) => {
   return {
     osm: {
       nameKey: "maps.maps.openStreetMap.name",
-      // mapboxStyle: "https://openmaptiles.geo.data.gouv.fr/styles/osm-bright/style.json",
       mapboxStyle: osmProtoMaps,
     },
     ign: {
@@ -45,10 +44,6 @@ export const mapStyleProvidersAtom = atom((get) => {
       // https://geoservices.ign.fr/documentation/services/services-geoplateforme/diffusion#70064
       // https://geoservices.ign.fr/documentation/services/api-et-services-ogc/tuiles-vectorielles-tmswmts/styles
       mapboxStyle: "https://data.geopf.fr/annexes/ressources/vectorTiles/styles/PLAN.IGN/standard.json",
-    },
-    ignClassique: {
-      nameKey: "maps.maps.ignClassique.name",
-      mapboxStyle: "https://data.geopf.fr/annexes/ressources/vectorTiles/styles/PLAN.IGN/classique.json",
     },
     ignSatellite: {
       nameKey: "maps.maps.ignSatellite.name",
